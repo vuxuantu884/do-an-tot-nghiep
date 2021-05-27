8a84443ccfa48b734c5cec546695ac8ffde46d15
IT. Nguyen Dinh Minh <minhnshd@gmail.com>
2021-05-27 08:18:00 +0700

    update

--- src/domain/sagas/index.ts
+++ src/domain/sagas/index.ts
@@ -3,13 +3,14 @@ import { appSaga } from './app.saga';
 import { authSaga } from './auth.saga';
 import { bootstrapSaga } from './bootstrap.saga';
 import { categorySaga } from './category.saga';
-
+import { productSaga } from './product.saga';
 function* rootSaga(){
     yield all([
       appSaga(),
       bootstrapSaga(),
       authSaga(),
       categorySaga(),
+      productSaga(),
     ]);
 }
 
---
+++ src/model/response/products/variant.prices.response.ts
@@ -0,0 +1,10 @@
+
+export interface VariantPricesResponse  {
+    id:number,
+    price_type:string,
+    price:number,
+    variant_id:number,
+    currency_code:string,
+    currency_symbol:string,
+    tax_percent:number
+  }
