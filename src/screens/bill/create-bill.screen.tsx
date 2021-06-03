21130c15372fe3026f9fdce01f9f008235fe1650
tungdv <dovantunghy186@gmail.com>
2021-06-03 22:32:54 +0700

    commit

--- src/screens/bill/create-bill.screen.tsx
+++ src/screens/bill/create-bill.screen.tsx
@@ -1,13 +1,4 @@
-import {
-  Button,
-  Card,
-  Input,
-  Row,
-  Col,
-  Tooltip,
-  Select,
-  Form,
-} from "antd";
+import { Button, Card, Input, Row, Col, Tooltip, Select, Form } from "antd";
 import documentIcon from "../../assets/img/document.svg";
 import warningCircleIcon from "assets/img/warning-circle.svg";
 import ProductCard from "../../component/OrderOnline/productCard";
@@ -17,15 +8,24 @@ import ShipmentCard from "../../component/OrderOnline/shipmentCard";
 import { useState, useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { StoreModel } from "model/other/StoreModel";
-import {
-  validateStoreAction,
-} from "domain/actions/core/store.action";
+import { validateStoreAction } from "domain/actions/core/store.action";
 import { AccountDetailResponse } from "model/response/accounts/account-detail.response";
 import { PageResponse } from "model/response/base-metadata.response";
 import AccountAction from "domain/actions/account/account.action";
 
 const CreateBill = () => {
+  const dispatch = useDispatch();
+  //State
   const [isVisibleAddress, setVisibleAddress] = useState(false);
+  const [isVisibleCustomer, setVisibleCustomer] = useState(false);
+  const [isVisibleBilling, setVisibleBilling] = useState(true);
+  const [isVerify, setVerify] = useState(false);
+  const [selectedShipMethod, setSelectedShipMethod] = useState(1);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(1);
+  const [store, setStore] = useState<StoreModel | null>(null);
+  const [accounts, setAccounts] = useState<Array<AccountDetailResponse>>([]);
+
+  //Address modal
   const showAddressModal = () => {
     setVisibleAddress(true);
   };
@@ -36,7 +36,7 @@ const CreateBill = () => {
     setVisibleAddress(false);
   }, []);
 
-  const [isVisibleCustomer, setVisibleCustomer] = useState(false);
+  //Customer modal
   const showCustomerModal = () => {
     setVisibleCustomer(true);
   };
@@ -47,27 +47,16 @@ const CreateBill = () => {
     setVisibleCustomer(false);
   }, []);
 
-  const [isVisibleBilling, setVisibleBilling] = useState(true);
+  //Bill Addresss
   const showBillingAddress = () => {
     setVisibleBilling(!isVisibleBilling);
   };
-
-  const [selectedShipMethod, setSelectedShipMethod] = useState(1);
   const changeShipMethod = (value: number) => {
     setSelectedShipMethod(value);
   };
-
-  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(1);
   const changePaymentMethod = (value: number) => {
     setSelectedPaymentMethod(value);
   };
-
-  const OrderItemModel = [{}];
-
-  const dispatch = useDispatch();
-  const [isVerify, setVerify] = useState(false);
-
-  const [store, setStore] = useState<StoreModel | null>(null);
 
   const onStoreSelect = useCallback(
     (item: number) => {
@@ -76,154 +65,155 @@ const CreateBill = () => {
     [dispatch]
   );
 
-  const [accounts, setAccounts] = useState<Array<AccountDetailResponse>>([]);
-  const setDataAccounts = useCallback((data: PageResponse<AccountDetailResponse>) => {
-    setAccounts(data.items);
-  }, []);
-
+  //Account dropdown
+  const setDataAccounts = useCallback(
+    (data: PageResponse<AccountDetailResponse>) => {
+      setAccounts(data.items);
+    },
+    []
+  );
   useEffect(() => {
     dispatch(AccountAction.SearchAccount({}, setDataAccounts));
   }, [dispatch, setDataAccounts]);
 
-  console.log("store", store);
-
   return (
     <div>
-      <Form></Form>
-      <Row gutter={24}>
-        <Col xs={24} lg={17}>
-          {/*--- customer ---*/}
-          <CustomerCard />
-          {/*--- end customer ---*/}
+      <Form>
+        <Row gutter={24}>
+          <Col xs={24} lg={17}>
+            {/*--- customer ---*/}
+            <CustomerCard />
+            {/*--- end customer ---*/}
 
-          {/*--- product ---*/}
-          <ProductCard select={onStoreSelect} />
-          {/*--- end product ---*/}
+            {/*--- product ---*/}
+            <ProductCard select={onStoreSelect} />
+            {/*--- end product ---*/}
 
-          {/*--- shipment ---*/}
-          <ShipmentCard />
-          {/*--- end shipment ---*/}
+            {/*--- shipment ---*/}
+            <ShipmentCard />
+            {/*--- end shipment ---*/}
 
-          {/*--- payment ---*/}
-          <PaymentCard />
-          {/*--- end payment ---*/}
-        </Col>
+            {/*--- payment ---*/}
+            <PaymentCard />
+            {/*--- end payment ---*/}
+          </Col>
 
-        <Col xs={24} lg={7}>
-          <Card
-            className="card-block card-block-normal"
-            title={
-              <div className="d-flex">
-                <img src={documentIcon} alt="" /> Thông tin đơn hàng
+          <Col xs={24} lg={7}>
+            <Card
+              className="card-block card-block-normal"
+              title={
+                <div className="d-flex">
+                  <img src={documentIcon} alt="" /> Thông tin đơn hàng
+                </div>
+              }
+            >
+              <div className="form-group form-group-with-search">
+                <label htmlFor="" className="required-label">
+                  Nhân viên bán hàng
+                </label>
+                <Select
+                  className="select-with-search"
+                  showSearch
+                  placeholder="Chọn nhân viên"
+                >
+                  {accounts.map((item, index) => (
+                    <Select.Option
+                      style={{ width: "100%" }}
+                      key={index}
+                      value={item.id}
+                    >
+                      {item.full_name}
+                    </Select.Option>
+                  ))}
+                </Select>
               </div>
-            }
-          >
-            <div className="form-group form-group-with-search">
-              <label htmlFor="" className="required-label">
-                Nhân viên bán hàng
-              </label>
-              <Select
-                className="select-with-search"
-                showSearch
-                placeholder="Chọn nhân viên"
-              >
-                {accounts.map((item, index) => (
-                  <Select.Option
-                    style={{ width: "100%" }}
-                    key={index}
-                    value={item.id}
+              <div className="form-group form-group-with-search">
+                <div>
+                  <label htmlFor="" className="">
+                    Tham chiếu
+                  </label>
+                  <Tooltip
+                    title="Thêm số tham chiếu hoặc ID đơn hàng gốc trên kênh bán hàng"
+                    className="tooltip-icon"
                   >
-                    {item.full_name}
-                  </Select.Option>
-                ))}
-              </Select>
-            </div>
-            <div className="form-group form-group-with-search">
-              <div>
-                <label htmlFor="" className="">
-                  Tham chiếu
-                </label>
-                <Tooltip
-                  title="Thêm số tham chiếu hoặc ID đơn hàng gốc trên kênh bán hàng"
-                  className="tooltip-icon"
-                >
-                  <span>
-                    <img src={warningCircleIcon} alt="" />
-                  </span>
-                </Tooltip>
+                    <span>
+                      <img src={warningCircleIcon} alt="" />
+                    </span>
+                  </Tooltip>
+                </div>
+                <Input placeholder="Điền tham chiếu" />
               </div>
-              <Input placeholder="Điền tham chiếu" />
-            </div>
-            <div className="form-group form-group-with-search mb-0">
-              <div>
-                <label htmlFor="" className="">
-                  Đường dẫn
-                </label>
-                <Tooltip
-                  title="Thêm đường dẫn đơn hàng gốc trên kênh bán hàng"
-                  className="tooltip-icon"
-                >
-                  <span>
-                    <img src={warningCircleIcon} alt="" />
-                  </span>
-                </Tooltip>
+              <div className="form-group form-group-with-search mb-0">
+                <div>
+                  <label htmlFor="" className="">
+                    Đường dẫn
+                  </label>
+                  <Tooltip
+                    title="Thêm đường dẫn đơn hàng gốc trên kênh bán hàng"
+                    className="tooltip-icon"
+                  >
+                    <span>
+                      <img src={warningCircleIcon} alt="" />
+                    </span>
+                  </Tooltip>
+                </div>
+                <Input placeholder="Điền đường dẫn" />
               </div>
-              <Input placeholder="Điền đường dẫn" />
-            </div>
-          </Card>
+            </Card>
 
-          <Card
-            className="card-block card-block-normal"
-            title={
-              <div className="d-flex">
-                <img src={documentIcon} alt="" /> Thông tin bổ sung
+            <Card
+              className="card-block card-block-normal"
+              title={
+                <div className="d-flex">
+                  <img src={documentIcon} alt="" /> Thông tin bổ sung
+                </div>
+              }
+            >
+              <div className="form-group form-group-with-search">
+                <div>
+                  <label htmlFor="" className="">
+                    Ghi chú
+                  </label>
+                  <Tooltip
+                    title="Thêm thông tin ghi chú chăm sóc khách hàng"
+                    className="tooltip-icon"
+                  >
+                    <span>
+                      <img src={warningCircleIcon} alt="" />
+                    </span>
+                  </Tooltip>
+                </div>
+                <Input.TextArea placeholder="Điền ghi chú" />
               </div>
-            }
-          >
-            <div className="form-group form-group-with-search">
-              <div>
-                <label htmlFor="" className="">
-                  Ghi chú
-                </label>
-                <Tooltip
-                  title="Thêm thông tin ghi chú chăm sóc khách hàng"
-                  className="tooltip-icon"
-                >
-                  <span>
-                    <img src={warningCircleIcon} alt="" />
-                  </span>
-                </Tooltip>
+              <div className="form-group form-group-with-search mb-0">
+                <div>
+                  <label htmlFor="" className="">
+                    Tag
+                  </label>
+                  <Tooltip
+                    title="Thêm từ khóa để tiện lọc đơn hàng"
+                    className="tooltip-icon"
+                  >
+                    <span>
+                      <img src={warningCircleIcon} alt="" />
+                    </span>
+                  </Tooltip>
+                </div>
+                <Input placeholder="Thêm tag" />
               </div>
-              <Input.TextArea placeholder="Điền ghi chú" />
-            </div>
-            <div className="form-group form-group-with-search mb-0">
-              <div>
-                <label htmlFor="" className="">
-                  Tag
-                </label>
-                <Tooltip
-                  title="Thêm từ khóa để tiện lọc đơn hàng"
-                  className="tooltip-icon"
-                >
-                  <span>
-                    <img src={warningCircleIcon} alt="" />
-                  </span>
-                </Tooltip>
-              </div>
-              <Input placeholder="Thêm tag" />
-            </div>
-          </Card>
-        </Col>
-      </Row>
+            </Card>
+          </Col>
+        </Row>
 
-      <Row className="footer-row-btn" justify="end">
-        <Button type="default" className="btn-style btn-cancel">
-          Hủy
-        </Button>
-        <Button type="default" className="btn-style btn-save">
-          Lưu
-        </Button>
-      </Row>
+        <Row className="footer-row-btn" justify="end">
+          <Button type="default" className="btn-style btn-cancel">
+            Hủy
+          </Button>
+          <Button type="default" className="btn-style btn-save">
+            Lưu
+          </Button>
+        </Row>
+      </Form>
     </div>
   );
 };
