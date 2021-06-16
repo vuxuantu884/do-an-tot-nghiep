edf6c8850bb095e5ef0f13b98973e8a415437c52
tungdv <dovantunghy186@gmail.com>
2021-06-16 14:56:32 +0700

    add

--- src/screens/order-online/payment-card.tsx
+++ src/screens/order-online/payment-card.tsx
@@ -8,105 +8,142 @@ import {
   Col,
   Radio,
   InputNumber,
-  Select
+  Select,
 } from "antd";
 
 // @ts-ignore
-import arrowDownIcon from 'assets/img/drow-down.svg';
+import arrowDownIcon from "assets/img/drow-down.svg";
 import walletIcon from "../../assets/img/wallet.svg";
 
 type PaymentCardProps = {
   setSelectedPaymentMethod: (paymentType: number) => void;
-  paymentMethod: number
-}
+  paymentMethod: number;
+};
 
 const PaymentCard: React.FC<PaymentCardProps> = (props: PaymentCardProps) => {
-
   const changePaymentMethod = (value: number) => {
     props.setSelectedPaymentMethod(value);
   };
 
   return (
-    <Card  className="margin-top-20"
-          title={<div className="d-flex"><img src={walletIcon} alt="" /> Thanh toán</div>}>
-      <div className="payment-method-radio-list">
-        <label htmlFor="" className="required-label"><i>Lựa chọn 1 hoặc nhiều hình thức thanh toán</i></label>
-        <div style={{ marginTop: 15 }}>
-          <Radio.Group name="radiogroup" value={props.paymentMethod} onChange={(e) => changePaymentMethod(e.target.value)}>
-            <Radio value={1}>COD</Radio>
-            <Radio value={2}>Thanh toán trước</Radio>
-            <Radio value={3}>Thanh toán sau</Radio>
-          </Radio.Group>
+    <Card
+      className="margin-top-20"
+      title={
+        <div className="d-flex">
+          <img src={walletIcon} alt="" /> Thanh toán
+        </div>
+      }
+    >
+      <div className="padding-20">
+        <div className="payment-method-radio-list">
+          <label htmlFor="" className="required-label">
+            <i>Lựa chọn 1 hoặc nhiều hình thức thanh toán</i>
+          </label>
+          <div style={{ marginTop: 15 }}>
+            <Radio.Group
+              name="radiogroup"
+              value={props.paymentMethod}
+              onChange={(e) => changePaymentMethod(e.target.value)}
+            >
+              <Radio value={1}>COD</Radio>
+              <Radio value={2}>Thanh toán trước</Radio>
+              <Radio value={3}>Thanh toán sau</Radio>
+            </Radio.Group>
+          </div>
+        </div>
+
+        <Divider />
+
+        <div className="payment-method-content">
+          <Row
+            gutter={24}
+            className="payment-cod-box"
+            hidden={props.paymentMethod !== 1}
+          >
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search">
+                <label htmlFor="" className="">
+                  Tiền thu hộ
+                </label>
+                <div>
+                  <InputNumber
+                    min={0}
+                    placeholder="Nhập số tiền"
+                    className="text-right hide-handler-wrap w-100"
+                  />
+                </div>
+              </div>
+            </Col>
+          </Row>
+
+          <Row gutter={24} hidden={props.paymentMethod !== 2}>
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search">
+                <label htmlFor="" className="">
+                  Hình thức thanh toán
+                </label>
+                {/*<Input placeholder="Chuyển Khoản"*/}
+                {/*       suffix={<img src={arrowDownIcon} alt="down" />}*/}
+                {/*/>*/}
+
+                <Select
+                  className="select-with-search"
+                  showSearch
+                  style={{ width: "100%" }}
+                  placeholder=""
+                >
+                  <Select.Option value="1">Chuyển khoản</Select.Option>
+                  <Select.Option value="2">COD</Select.Option>
+                  <Select.Option value="3">Communicated</Select.Option>
+                  <Select.Option value="4">Identified</Select.Option>
+                </Select>
+              </div>
+            </Col>
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search">
+                <label htmlFor="" className="">
+                  Số tiền
+                </label>
+                <Input placeholder="Chuyển Khoản" />
+              </div>
+            </Col>
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search">
+                <label htmlFor="" className="">
+                  Ngày chuyển khoản
+                </label>
+                <Input
+                  placeholder="Ngày chuyển khoản"
+                  suffix={<img src={arrowDownIcon} alt="down" />}
+                />
+              </div>
+            </Col>
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search">
+                <label htmlFor="" className="">
+                  Tham chiếu
+                </label>
+                <Input placeholder="Nhập tham chiếu" />
+              </div>
+            </Col>
+            <Col span={24}>
+              <Button type="link" className="p-0">
+                Thêm hình thức thanh toán
+              </Button>
+            </Col>
+          </Row>
+
+          <Row className="payment-later-box" hidden={props.paymentMethod !== 3}>
+            <div className="form-group m-0">
+              <label htmlFor="">
+                <i>Bạn có thể xử lý thanh toán sau khi tạo đơn hàng.</i>
+              </label>
+            </div>
+          </Row>
         </div>
       </div>
-
-      <Divider/>
-
-      <div className="payment-method-content">
-        <Row gutter={24} className="payment-cod-box" hidden={props.paymentMethod !== 1}>
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search">
-              <label htmlFor="" className="">Tiền thu hộ</label>
-              <div>
-                <InputNumber min={0} placeholder="Nhập số tiền" className="text-right hide-handler-wrap w-100"/>
-              </div>
-            </div>
-          </Col>
-        </Row>
-
-        <Row gutter={24} hidden={props.paymentMethod !== 2}>
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search">
-              <label htmlFor="" className="">Hình thức thanh toán</label>
-              {/*<Input placeholder="Chuyển Khoản"*/}
-              {/*       suffix={<img src={arrowDownIcon} alt="down" />}*/}
-              {/*/>*/}
-
-              <Select className="select-with-search" showSearch
-                      style={{ width: '100%' }}
-                      placeholder=""
-              >
-                <Select.Option value="1">Chuyển khoản</Select.Option>
-                <Select.Option value="2">COD</Select.Option>
-                <Select.Option value="3">Communicated</Select.Option>
-                <Select.Option value="4">Identified</Select.Option>
-              </Select>
-            </div>
-          </Col>
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search">
-              <label htmlFor="" className="">Số tiền</label>
-              <Input placeholder="Chuyển Khoản"/>
-            </div>
-          </Col>
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search">
-              <label htmlFor="" className="">Ngày chuyển khoản</label>
-              <Input placeholder="Ngày chuyển khoản"
-                     suffix={<img src={arrowDownIcon} alt="down" />}
-              />
-            </div>
-          </Col>
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search">
-              <label htmlFor="" className="">Tham chiếu</label>
-              <Input placeholder="Nhập tham chiếu"/>
-            </div>
-          </Col>
-          <Col span={24}>
-            <Button type="link" className="p-0">Thêm hình thức thanh toán</Button>
-          </Col>
-        </Row>
-
-        <Row className="payment-later-box" hidden={props.paymentMethod !== 3}>
-          <div className="form-group m-0">
-            <label htmlFor=""><i>Bạn có thể xử lý thanh toán sau khi tạo đơn hàng.</i></label>
-          </div>
-        </Row>
-      </div>
-
     </Card>
-  )
-}
+  );
+};
 
 export default PaymentCard;
--- src/screens/order-online/shipment-card.tsx
+++ src/screens/order-online/shipment-card.tsx
@@ -50,224 +50,226 @@ const ShipmentCard: React.FC<ShipmentCardProps> = (
         </div>
       }
     >
-      <Row gutter={24} className="">
-        <Col xs={24} lg={12}>
-          <div>
-            <label htmlFor="" className="required-label">
-              <i>Lựa chọn 1 trong hình thức giao hàng</i>
-            </label>
-          </div>
-          <div style={{ marginTop: 15 }}>
-            <Radio.Group
-              value={props.shipmentMethod}
-              onChange={(e) => changeShipMethod(e.target.value)}
-            >
-              <Space direction="vertical">
-                <Radio value={1}>Chuyển đối tác giao hàng</Radio>
-                <Radio value={2}>Tự giao hàng</Radio>
-                <Radio value={3}>Nhận tại cửa hàng</Radio>
-                <Radio value={4}>Giao hàng sau</Radio>
-              </Space>
-            </Radio.Group>
-          </div>
-        </Col>
+      <div className="padding-20">
+        <Row gutter={24} className="">
+          <Col xs={24} lg={12}>
+            <div>
+              <label htmlFor="" className="required-label">
+                <i>Lựa chọn 1 trong hình thức giao hàng</i>
+              </label>
+            </div>
+            <div style={{ marginTop: 15 }}>
+              <Radio.Group
+                value={props.shipmentMethod}
+                onChange={(e) => changeShipMethod(e.target.value)}
+              >
+                <Space direction="vertical">
+                  <Radio value={1}>Chuyển đối tác giao hàng</Radio>
+                  <Radio value={2}>Tự giao hàng</Radio>
+                  <Radio value={3}>Nhận tại cửa hàng</Radio>
+                  <Radio value={4}>Giao hàng sau</Radio>
+                </Space>
+              </Radio.Group>
+            </div>
+          </Col>
 
-        <Col xs={24} lg={12}>
-          <div className="form-group form-group-with-search">
-            <label htmlFor="" className="">
-              Hẹn giao
-            </label>
-            <Input
-              placeholder="Chọn ngày giao"
-              suffix={<img src={arrowDownIcon} alt="down" />}
-            />
-          </div>
-          <div className="form-group form-group-with-search">
-            <label htmlFor="" className="">
-              Yêu cầu
-            </label>
-            <Input
-              placeholder="Cho phép xem hàng và thử hàng"
-              suffix={<img src={arrowDownIcon} alt="down" />}
-            />
-          </div>
-        </Col>
-      </Row>
-
-      <Divider />
-
-      <div>
-        {/*--- đối tác ----*/}
-        <Row
-          gutter={24}
-          className="ship-box"
-          hidden={props.shipmentMethod !== 1}
-        >
           <Col xs={24} lg={12}>
             <div className="form-group form-group-with-search">
               <label htmlFor="" className="">
-                Phí ship báo khách
+                Hẹn giao
               </label>
-              <InputNumber
-                placeholder=""
-                className="text-right hide-handler-wrap w-100"
+              <Input
+                placeholder="Chọn ngày giao"
+                suffix={<img src={arrowDownIcon} alt="down" />}
+              />
+            </div>
+            <div className="form-group form-group-with-search">
+              <label htmlFor="" className="">
+                Yêu cầu
+              </label>
+              <Input
+                placeholder="Cho phép xem hàng và thử hàng"
+                suffix={<img src={arrowDownIcon} alt="down" />}
               />
             </div>
           </Col>
-
-          <Col span={24}>
-            <div className="table-ship w-100">
-              <div className="custom-table">
-                <table className="w-100">
-                  <thead>
-                    <tr>
-                      <th>Hãng vận chuyển</th>
-                      <th>Dịch vụ chuyển phát</th>
-                      <th>Cước phí</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <img src={dhlIcon} alt="" />
-                      </td>
-                      <td>
-                        <Radio value={1}>Chuyển phát nhanh PDE</Radio>
-                      </td>
-                      <td>
-                        <Typography.Text type="success">
-                          {formatCurrency(18000)}
-                        </Typography.Text>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td style={{ borderBottom: 0 }}>
-                        <img src={ghtkIcon} alt="" />
-                      </td>
-                      <td>
-                        <Radio value={2}>Đường bộ</Radio>
-                      </td>
-                      <td>{formatCurrency(30000)}</td>
-                    </tr>
-
-                    <tr>
-                      <td style={{ borderTop: 0 }} />
-                      <td>
-                        <Radio value={3}>Đường bay</Radio>
-                      </td>
-                      <td>{formatCurrency(50000)}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </Col>
         </Row>
 
-        {/*--- Tự giao hàng ----*/}
-        <Row
-          gutter={24}
-          className="ship-cod"
-          hidden={props.shipmentMethod !== 2}
-        >
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search form-search-customer">
-              <label htmlFor="" className="">
-                Đối tác giao hàng
-              </label>
-              <div>
-                <AutoComplete>
-                  <Input.Search
-                    placeholder="Chọn đối tác giao hàng"
-                    enterButton={
-                      <Button type="text">
-                        <img src={plusBlueIcon} alt="" />
-                      </Button>
-                    }
-                    prefix={<SearchOutlined style={{ color: "#ABB4BD" }} />}
-                    onSearch={() => console.log(1)}
-                    suffix={<img src={arrowDownIcon} alt="down" />}
+        <Divider />
+
+        <div>
+          {/*--- đối tác ----*/}
+          <Row
+            gutter={24}
+            className="ship-box"
+            hidden={props.shipmentMethod !== 1}
+          >
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search">
+                <label htmlFor="" className="">
+                  Phí ship báo khách
+                </label>
+                <InputNumber
+                  placeholder=""
+                  className="text-right hide-handler-wrap w-100"
+                />
+              </div>
+            </Col>
+
+            <Col span={24}>
+              <div className="table-ship w-100">
+                <div className="custom-table">
+                  <table className="w-100">
+                    <thead>
+                      <tr>
+                        <th>Hãng vận chuyển</th>
+                        <th>Dịch vụ chuyển phát</th>
+                        <th>Cước phí</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <img src={dhlIcon} alt="" />
+                        </td>
+                        <td>
+                          <Radio value={1}>Chuyển phát nhanh PDE</Radio>
+                        </td>
+                        <td>
+                          <Typography.Text type="success">
+                            {formatCurrency(18000)}
+                          </Typography.Text>
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td style={{ borderBottom: 0 }}>
+                          <img src={ghtkIcon} alt="" />
+                        </td>
+                        <td>
+                          <Radio value={2}>Đường bộ</Radio>
+                        </td>
+                        <td>{formatCurrency(30000)}</td>
+                      </tr>
+
+                      <tr>
+                        <td style={{ borderTop: 0 }} />
+                        <td>
+                          <Radio value={3}>Đường bay</Radio>
+                        </td>
+                        <td>{formatCurrency(50000)}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </Col>
+          </Row>
+
+          {/*--- Tự giao hàng ----*/}
+          <Row
+            gutter={24}
+            className="ship-cod"
+            hidden={props.shipmentMethod !== 2}
+          >
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search form-search-customer">
+                <label htmlFor="" className="">
+                  Đối tác giao hàng
+                </label>
+                <div>
+                  <AutoComplete>
+                    <Input.Search
+                      placeholder="Chọn đối tác giao hàng"
+                      enterButton={
+                        <Button type="text">
+                          <img src={plusBlueIcon} alt="" />
+                        </Button>
+                      }
+                      prefix={<SearchOutlined style={{ color: "#ABB4BD" }} />}
+                      onSearch={() => console.log(1)}
+                      suffix={<img src={arrowDownIcon} alt="down" />}
+                    />
+                  </AutoComplete>
+                </div>
+              </div>
+            </Col>
+
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search form-search-customer">
+                <label htmlFor="" className="">
+                  Phí ship báo khách
+                </label>
+                <div>
+                  <InputNumber
+                    placeholder="Nhập số tiền"
+                    className="text-right hide-handler-wrap w-100"
                   />
-                </AutoComplete>
+                </div>
               </div>
-            </div>
-          </Col>
+            </Col>
 
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search form-search-customer">
-              <label htmlFor="" className="">
-                Phí ship báo khách
-              </label>
-              <div>
-                <InputNumber
-                  placeholder="Nhập số tiền"
-                  className="text-right hide-handler-wrap w-100"
-                />
+            <Col xs={24} lg={12}>
+              <div className="form-group form-group-with-search form-search-customer">
+                <label htmlFor="" className="">
+                  Phí ship trả đối tác giao hàng
+                </label>
+                <div>
+                  <InputNumber
+                    placeholder="Nhập số tiền"
+                    className="text-right hide-handler-wrap w-100"
+                  />
+                </div>
               </div>
-            </div>
-          </Col>
-
-          <Col xs={24} lg={12}>
-            <div className="form-group form-group-with-search form-search-customer">
-              <label htmlFor="" className="">
-                Phí ship trả đối tác giao hàng
-              </label>
-              <div>
-                <InputNumber
-                  placeholder="Nhập số tiền"
-                  className="text-right hide-handler-wrap w-100"
-                />
-              </div>
-            </div>
-          </Col>
-        </Row>
-
-        {/*--- Nhận tại cửa hàng ----*/}
-        <div className="receive-at-store" hidden={props.shipmentMethod !== 3}>
-          <Row>Nhận tại cửa hàng</Row>
-          <Row className="row-info">
-            <Space>
-              <div className="row-info-icon">
-                <img src={storeBluecon} alt="" />
-              </div>
-              <div className="row-info-title">Cửa hàng</div>
-              <div className="row-info-content">
-                <Typography.Link>YODY Kho Online</Typography.Link>
-              </div>
-            </Space>
+            </Col>
           </Row>
-          <Row className="row-info">
-            <Space>
-              <div className="row-info-icon">
-                <img src={callIcon} alt="" />
-              </div>
-              <div className="row-info-title">Điện thoại</div>
-              <div className="row-info-content">0968563666</div>
-            </Space>
-          </Row>
-          <Row className="row-info">
-            <Space>
-              <div className="row-info-icon">
-                <img src={locationIcon} alt="" />
-              </div>
-              <div className="row-info-title">Địa chỉ</div>
-              <div className="row-info-content">
-                Khu Tiểu Thủ CN Gia Xuyên - Phố ĐInh Lễ - Xã Gia Xuyên - TP Hải
-                Dương
-              </div>
-            </Space>
+
+          {/*--- Nhận tại cửa hàng ----*/}
+          <div className="receive-at-store" hidden={props.shipmentMethod !== 3}>
+            <Row>Nhận tại cửa hàng</Row>
+            <Row className="row-info">
+              <Space>
+                <div className="row-info-icon">
+                  <img src={storeBluecon} alt="" />
+                </div>
+                <div className="row-info-title">Cửa hàng</div>
+                <div className="row-info-content">
+                  <Typography.Link>YODY Kho Online</Typography.Link>
+                </div>
+              </Space>
+            </Row>
+            <Row className="row-info">
+              <Space>
+                <div className="row-info-icon">
+                  <img src={callIcon} alt="" />
+                </div>
+                <div className="row-info-title">Điện thoại</div>
+                <div className="row-info-content">0968563666</div>
+              </Space>
+            </Row>
+            <Row className="row-info">
+              <Space>
+                <div className="row-info-icon">
+                  <img src={locationIcon} alt="" />
+                </div>
+                <div className="row-info-title">Địa chỉ</div>
+                <div className="row-info-content">
+                  Khu Tiểu Thủ CN Gia Xuyên - Phố ĐInh Lễ - Xã Gia Xuyên - TP
+                  Hải Dương
+                </div>
+              </Space>
+            </Row>
+          </div>
+
+          {/*--- Giao hàng sau ----*/}
+          <Row className="ship-later-box" hidden={props.shipmentMethod !== 4}>
+            <div className="form-group m-0">
+              <label htmlFor="">
+                <i>Bạn có thể xử lý giao hàng sau khi tạo và duyệt đơn hàng.</i>
+              </label>
+            </div>
           </Row>
         </div>
-
-        {/*--- Giao hàng sau ----*/}
-        <Row className="ship-later-box" hidden={props.shipmentMethod !== 4}>
-          <div className="form-group m-0">
-            <label htmlFor="">
-              <i>Bạn có thể xử lý giao hàng sau khi tạo và duyệt đơn hàng.</i>
-            </label>
-          </div>
-        </Row>
       </div>
     </Card>
   );
