85cff33f83c0032013327798e17566250787ac4a
Anh Nguyen <anhdev97@gmail.com>
2021-07-20 15:34:17 +0700

    add po form

--- src/screens/purchase-order/purchase-order-create.screen.tsx
+++ src/screens/purchase-order/purchase-order-create.screen.tsx
@@ -1,77 +1,23 @@
 import {
-  UserOutlined,
-  SearchOutlined,
-  UnorderedListOutlined,
   CheckOutlined,
 } from "@ant-design/icons";
 import {
   Button,
-  Card,
-  Input,
   Row,
   Col,
   Form,
-  Checkbox,
   Space,
-  AutoComplete,
   Steps,
 } from "antd";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { useDispatch } from "react-redux";
+import {useEffect} from "react";
 import POSupplierForm from "./component/po-supplier.form";
-import PurchaseItem from "./component/purchase-item";
 import PurchaseInfo from "./component/purchase-info";
-import { SupplierQuery, SupplierResponse } from "model/core/supplier.model";
-import { PageResponse } from "model/base/base-metadata.response";
-import { SupplierSearchAction } from "domain/actions/core/supplier.action";
 import ContentContainer from "component/container/content.container";
 import UrlConfig from "config/UrlConfig";
-
-const initSupplierQuery: SupplierQuery = {
-  limit: 10,
-  page: 1,
-};
+import POProductForm from "./component/po-product.form";
 
 const POCreateScreen = () => {
-  const dispatch = useDispatch();
-  const [listSupplier, setListSupplier] = useState<Array<SupplierResponse>>([]);
 
-  const setDataSupplier = useCallback(
-    (data: PageResponse<SupplierResponse>) => {
-      setListSupplier(data.items);
-    },
-    []
-  );
-  const OptionRenderAutocomplete = (item: SupplierResponse) => {
-    return (
-      <div className="row-search w-100">
-        <div className="rs-left w-100">
-          {/* <img src={imgdefault} alt="anh" placeholder={imgdefault} /> */}
-          <div className="rs-info w-100">
-            <span className="text">
-              {item.name} - {item.phone}
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  };
-  const supplierOptionAutocomplete = useMemo(() => {
-    let options: any[] = [];
-    listSupplier.forEach((item: SupplierResponse, index: number) => {
-      options.push({
-        label: OptionRenderAutocomplete(item),
-        value: item.id ? item.id.toString() : "",
-      });
-    });
-    return options;
-  }, [listSupplier]);
-
-  const onChangeSearchSupplier = (val: string) => {
-    initSupplierQuery.info = val;
-
-    dispatch(SupplierSearchAction(initSupplierQuery, setDataSupplier));
-  };
   useEffect(() => {}, []);
 
   return (
@@ -114,34 +60,7 @@ const POCreateScreen = () => {
           {/* Left Side */}
           <Col md={18}>
             <POSupplierForm />
-            <Card
-              className="margin-top-20"
-              title={
-                <Space>
-                  <UnorderedListOutlined />
-                  Sản phẩm
-                </Space>
-              }
-              extra={
-                <Space size={20}>
-                  <Checkbox>Tách dòng</Checkbox>
-                </Space>
-              }
-            >
-              <div className="padding-20">
-                <Row gutter={20}>
-                  <Col md={24}>
-                    <Form.Item label="Sản phẩm">
-                      <Input
-                        prefix={<SearchOutlined />}
-                        placeholder="Tìm sản phẩm/ SKU/ mã vạch (F3)"
-                      />
-                    </Form.Item>
-                  </Col>
-                </Row>
-              </div>
-              <PurchaseItem />
-            </Card>
+            <POProductForm />
           </Col>
           {/* Right Side */}
           <Col md={6}>
