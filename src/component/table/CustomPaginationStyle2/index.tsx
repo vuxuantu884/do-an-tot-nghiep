d7c4e80cc31f5ad49f2ba7e74f54bf25c2815834
manhnguyengg@gmail.com <manhnv@sunshinegroup.vn>
2021-07-29 20:20:20 +0700

    tach pagination2

--- src/component/table/CustomPaginationStyle2/index.tsx
+++ src/component/table/CustomPaginationStyle2/index.tsx
@@ -1,80 +1,164 @@
-import { DownOutlined } from "@ant-design/icons";
-import { Dropdown, Menu, Pagination } from "antd";
+import { DoubleLeftOutlined, DoubleRightOutlined } from "@ant-design/icons";
+import { Col, Pagination, Row, Select, TableProps } from "antd";
+import classNames from "classnames";
 import { PageConfig } from "config/PageConfig";
-import { BaseMetadata } from "model/base/base-metadata.response";
-import React, { useMemo } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { StyledComponent } from "./styles";
 
-type CustomPaginationProps = {
-  metadata: BaseMetadata;
-  onChange?: (size: number, page: number) => void
+export interface ICustomPaginationProps
+  extends Omit<TableProps<any>, "pagination"> {
+  pagination?: false | ICustomTablePaginationConfig;
+}
+
+export interface ICustomTablePaginationConfig {
+  total: number;
+  disabled?: boolean;
+  current?: number;
+  pageSize: number;
+  onChange?: (page: number, pageSize?: number) => void;
+  showSizeChanger?: boolean;
+  pageSizeOptions?: string[];
+  onShowSizeChange?: (current: number, size: number) => void;
+  style?: React.CSSProperties;
+  className?: string;
+}
+const defaultPagination: ICustomTablePaginationConfig = {
+  current: 1,
+  total: 1,
+  pageSize: 50,
+  showSizeChanger: true,
+  pageSizeOptions: PageConfig,
 };
 
-const CustomPaginationStyle2: React.FC<CustomPaginationProps> = (
-  props: CustomPaginationProps
+const showTotal = (pagination: ICustomTablePaginationConfig) => {
+  let { total = 1, current = 1, pageSize = 1 } = pagination;
+  let from = (current - 1) * pageSize + 1;
+  let to = current * pageSize;
+  if (from > total) {
+    return "Không có kết quả";
+  }
+  if (to > total) to = total;
+  return `${from} - ${to} trong tổng ${total}`;
+};
+
+const handleLastNextPage = (
+  pagination: ICustomTablePaginationConfig,
+  type: number
 ) => {
-  const { metadata, onChange } = props;
-  const numPage = useMemo(() => Math.floor(metadata.total / metadata.limit), [metadata.limit, metadata.total])
-  const result = useMemo(() => {
-    if (metadata.total <= 1) {
-      return metadata.total;
-    }
-    let from = metadata.page * metadata.limit + 1;
-    let to = (metadata.page + 1) * metadata.limit
-    if (numPage === metadata.page) {
-      let rest = metadata.total - from;
-      to = from + rest;
-    }
-    return `${from} - ${to}`;
-  }, [metadata.limit, metadata.page, metadata.total, numPage]);
+  const { current = 1, total = 1, pageSize = 1, onChange } = pagination;
+  const totalPage = Math.ceil(total / pageSize);
+  if (!onChange) return;
+
+  if (type) {
+    if (current === totalPage) return;
+    return onChange(totalPage, pageSize);
+  }
+
+  if (current === 1) return;
+  return onChange(1, pageSize);
+};
+
+const handleSizeChanger = (
+  pagination: ICustomTablePaginationConfig,
+  value: number
+) => {
+  const { current = 1, onShowSizeChange } = pagination;
+  return onShowSizeChange && onShowSizeChange(current, value);
+};
+
+const CustomPaginationStyle2 = (props: ICustomPaginationProps) => {
+  const { pagination = defaultPagination } = props;
+
+  const totalPage = pagination
+    ? Math.ceil((pagination.total || 1) / (pagination.pageSize || 1))
+    : 1;
   return (
-    <div className="yody-pagination yody-pagination-text">
-      {metadata.total !== 0 && (
-        <React.Fragment>
-          <span>
-            Hiện thị kết quả: {" "}
-            <span className="yody-pagination-bold">
-              {result}
-            </span>
-            /<span>{metadata.total}</span> kết quả
-            </span>
-          {
-            numPage > 0 && (
-              <React.Fragment>
-                <div className="pagination-right">
-                  <div className="page-size-view">
-                    Hiển thị số dòng:
-                    <Dropdown trigger={["click"]} placement="bottomCenter" overlay={
-                      <Menu>
-                        {
-                          PageConfig.map((item, index) => (
-                            <Menu.Item disabled={metadata.limit.toString() === item} key={index} onClick={() => onChange && onChange(parseInt(item), metadata.page)}>{item.toString()}</Menu.Item>
-                          ))
-                        }
-                      </Menu>
-                    }>
-                      <Link to="#" className="page-size-selected" onClick={e => e.preventDefault()}>
-                        <div style={{ color: '#2A2A86' }}>{metadata.limit}</div><DownOutlined />
-                      </Link>
-                    </Dropdown>
-                  </div>
-                  <Pagination
-                    hideOnSinglePage
-                    size="small"
-                    pageSize={metadata.limit}
-                    current={metadata.page + 1}
-                    total={metadata.total}
-                    className="yody-page"
-                    onChange={(page, pageSize) => onChange && onChange(metadata.limit, page - 1)}
-                  />
+    <StyledComponent>
+      {pagination && (
+        <div className="pagination">
+          <Row>
+            <Col span={7}>
+              <span className="pagination__showTotal">
+                {showTotal(pagination)}
+              </span>
+            </Col>
+            <Col span={7}>
+              {pagination.showSizeChanger && (
+                <div className="pagination__sizeChange">
+                  <label
+                    htmlFor="custom-pagination-size-changer"
+                    style={{ marginRight: 12 }}
+                  >
+                    Hiển thị:{" "}
+                  </label>
+                  <Select
+                    value={pagination.pageSize}
+                    id="custom-pagination-size-changer"
+                    onChange={(value: number) =>
+                      handleSizeChanger(pagination, value)
+                    }
+                  >
+                    {pagination &&
+                      PageConfig.map((size) => (
+                        <Select.Option key={size} value={size}>
+                          {size}
+                        </Select.Option>
+                      ))}
+                  </Select>
+                  <span style={{ marginLeft: 12 }}>Kết quả</span>
                 </div>
-              </React.Fragment>
-            )
-          }
-        </React.Fragment>
+              )}
+            </Col>
+            <Col span={10}>
+              <div className="pagination__main">
+                <div title="Trang đầu" className="ant-pagination-first">
+                  <button
+                    onClick={() => handleLastNextPage(pagination, 0)}
+                    className={classNames(
+                      "ant-pagination-item ant-pagination-item-link",
+                      pagination.current &&
+                        pagination.current === 1 &&
+                        "ant-pagination-disabled"
+                    )}
+                    type="button"
+                  >
+                    <DoubleLeftOutlined style={{ marginRight: 10 }} />
+                    Trang đầu
+                  </button>
+                </div>
+                <Pagination
+                  total={pagination.total}
+                  current={pagination.current}
+                  pageSize={pagination.pageSize}
+                  onChange={pagination.onChange}
+                  showSizeChanger={false}
+                />
+                <div
+                  title="Trang cuối"
+                  className="ant-pagination-last"
+                  style={{ marginLeft: 8 }}
+                >
+                  <button
+                    onClick={() => handleLastNextPage(pagination, 1)}
+                    className={classNames(
+                      "ant-pagination-item ant-pagination-item-link",
+                      pagination.current &&
+                        pagination.current === totalPage &&
+                        "ant-pagination-disabled"
+                    )}
+                    type="button"
+                  >
+                    Trang cuối
+                    <DoubleRightOutlined style={{ marginLeft: 10 }} />
+                  </button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
       )}
-    </div>
+    </StyledComponent>
   );
 };
 
-export default CustomPaginationStyle2;
+export default React.memo(CustomPaginationStyle2);
--- src/component/table/CustomTableStyle2/index.tsx
+++ src/component/table/CustomTableStyle2/index.tsx
@@ -1,23 +1,9 @@
-import {
-  DoubleLeftOutlined,
-  DoubleRightOutlined,
-  LoadingOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import {
-  Button,
-  Col,
-  Pagination,
-  Row,
-  Select,
-  Spin,
-  Table as ANTTable,
-  TableProps,
-} from "antd";
+import { LoadingOutlined, SettingOutlined } from "@ant-design/icons";
+import { Button, Spin, Table as ANTTable, TableProps } from "antd";
 import { ColumnType, TableLocale } from "antd/lib/table/interface";
-import classNames from "classnames";
 import { PageConfig } from "config/PageConfig";
 import React, { useCallback } from "react";
+import CustomPaginationStyle2 from "../CustomPaginationStyle2";
 import { StyledComponent } from "./styles";
 
 export interface ICustomTableProps extends Omit<TableProps<any>, "pagination"> {
@@ -65,42 +51,6 @@ const defaultPagination: ICustomTablePaginationConfig = {
   pageSizeOptions: PageConfig,
 };
 
-const showTotal = (pagination: ICustomTablePaginationConfig) => {
-  let { total = 1, current = 1, pageSize = 1 } = pagination;
-  let from = (current - 1) * pageSize + 1;
-  let to = current * pageSize;
-  if (from > total) {
-    return "Không có kết quả";
-  }
-  if (to > total) to = total;
-  return `${from} - ${to} trong tổng ${total}`;
-};
-
-const handleLastNextPage = (
-  pagination: ICustomTablePaginationConfig,
-  type: number
-) => {
-  const { current = 1, total = 1, pageSize = 1, onChange } = pagination;
-  const totalPage = Math.ceil(total / pageSize);
-  if (!onChange) return;
-
-  if (type) {
-    if (current === totalPage) return;
-    return onChange(totalPage, pageSize);
-  }
-
-  if (current === 1) return;
-  return onChange(1, pageSize);
-};
-
-const handleSizeChanger = (
-  pagination: ICustomTablePaginationConfig,
-  value: number
-) => {
-  const { current = 1, onShowSizeChange } = pagination;
-  return onShowSizeChange && onShowSizeChange(current, value);
-};
-
 const CustomTableStyle2 = (props: ICustomTableProps) => {
   const {
     locale = defaultLocale,
@@ -114,9 +64,6 @@ const CustomTableStyle2 = (props: ICustomTableProps) => {
     isBordered,
   } = props;
 
-  const totalPage = pagination
-    ? Math.ceil((pagination.total || 1) / (pagination.pageSize || 1))
-    : 1;
   const configSettingColumns: ICustomTableColumType<any>[] = [
     {
       title: (
@@ -179,89 +126,7 @@ const CustomTableStyle2 = (props: ICustomTableProps) => {
           // scroll={{ y: 700 }}
           size="middle"
         />
-        {pagination && (
-          <div className="pagination">
-            <Row>
-              <Col span={7}>
-                <span className="pagination__showTotal">
-                  {showTotal(pagination)}
-                </span>
-              </Col>
-              <Col span={7}>
-                {pagination.showSizeChanger && (
-                  <div className="pagination__sizeChange">
-                    <label
-                      htmlFor="custom-pagination-size-changer"
-                      style={{ marginRight: 12 }}
-                    >
-                      Hiển thị:{" "}
-                    </label>
-                    <Select
-                      value={pagination.pageSize}
-                      id="custom-pagination-size-changer"
-                      onChange={(value: number) =>
-                        handleSizeChanger(pagination, value)
-                      }
-                    >
-                      {pagination &&
-                        PageConfig.map((size) => (
-                          <Select.Option key={size} value={size}>
-                            {size}
-                          </Select.Option>
-                        ))}
-                    </Select>
-                    <span style={{ marginLeft: 12 }}>Kết quả</span>
-                  </div>
-                )}
-              </Col>
-              <Col span={10}>
-                <div className="pagination__main">
-                  <div title="Trang đầu" className="ant-pagination-first">
-                    <button
-                      onClick={() => handleLastNextPage(pagination, 0)}
-                      className={classNames(
-                        "ant-pagination-item ant-pagination-item-link",
-                        pagination.current &&
-                          pagination.current === 1 &&
-                          "ant-pagination-disabled"
-                      )}
-                      type="button"
-                    >
-                      <DoubleLeftOutlined style={{ marginRight: 10 }} />
-                      Trang đầu
-                    </button>
-                  </div>
-                  <Pagination
-                    total={pagination.total}
-                    current={pagination.current}
-                    pageSize={pagination.pageSize}
-                    onChange={pagination.onChange}
-                    showSizeChanger={false}
-                  />
-                  <div
-                    title="Trang cuối"
-                    className="ant-pagination-last"
-                    style={{ marginLeft: 8 }}
-                  >
-                    <button
-                      onClick={() => handleLastNextPage(pagination, 1)}
-                      className={classNames(
-                        "ant-pagination-item ant-pagination-item-link",
-                        pagination.current &&
-                          pagination.current === totalPage &&
-                          "ant-pagination-disabled"
-                      )}
-                      type="button"
-                    >
-                      Trang cuối
-                      <DoubleRightOutlined style={{ marginLeft: 10 }} />
-                    </button>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        )}
+        {pagination && <CustomPaginationStyle2 pagination={pagination} />}
       </div>
     </StyledComponent>
   );
