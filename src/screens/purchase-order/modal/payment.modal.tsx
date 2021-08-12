b4283068d14904987443d165223aa06751c4efd3
IT. Nguyen Dinh Minh <minhnshd@gmail.com>
2021-08-12 13:46:22 +0700

    fix bug

--- src/screens/purchase-order/modal/payment.modal.tsx
+++ src/screens/purchase-order/modal/payment.modal.tsx
@@ -28,6 +28,7 @@ const PaymentModal: React.FC<PaymentModalProps> = (
   const { poId, purchasePayment, visible, onCancel, onOk } = props;
   const [formPayment] = Form.useForm();
   const [confirmLoading, setConfirmLoading] = React.useState(false);
+  const [disabledRef, setDisabledRef] = React.useState(false);
 
   const onOkPress = useCallback(() => {
     // onOk();
@@ -63,7 +64,7 @@ const PaymentModal: React.FC<PaymentModalProps> = (
     (values: PurchasePayments) => {
       setConfirmLoading(true);
       let data = formPayment.getFieldsValue(true);
-      
+
       if (data.id) {
         dispatch(PoPaymentUpdateAction(poId, data.id, data, updateCallback));
       } else {
@@ -74,6 +75,18 @@ const PaymentModal: React.FC<PaymentModalProps> = (
     [createCallback, dispatch, formPayment, poId, updateCallback]
   );
 
+  const onChangePaymentMethod = (e:any) => {
+    debugger;
+    console.log('radio checked', e.target.value);
+    if(e.target.value ===PoPaymentMethod.BANK_TRANSFER){
+setDisabledRef(false);
+    }else{
+      formPayment.setFieldsValue({ reference: '' });
+      setDisabledRef(true);
+    }
+    // setValue(e.target.value);
+  };
+
   const prevVisibleRef = useRef(false);
   useEffect(() => {
     prevVisibleRef.current = visible;
@@ -81,13 +94,11 @@ const PaymentModal: React.FC<PaymentModalProps> = (
   const prevVisible = prevVisibleRef.current;
   useEffect(() => {
     if (!visible && prevVisible) {
-      
       formPayment.resetFields();
     }
   }, [formPayment, prevVisible, visible]);
   useEffect(() => {
     if (visible) {
-      
       if (purchasePayment) {
         formPayment.setFieldsValue(purchasePayment);
       }
@@ -121,10 +132,10 @@ const PaymentModal: React.FC<PaymentModalProps> = (
                   message: "Vui lòng chọn phương thức thanh toán",
                 },
               ]}
-              label="Hình thức thanh toán"
+              label="Phương thức thanh toán"
               name="payment_method_code"
             >
-              <Radio.Group>
+              <Radio.Group onChange={onChangePaymentMethod}>
                 <Radio
                   value={PoPaymentMethod.BANK_TRANSFER}
                   key={PoPaymentMethod.BANK_TRANSFER}
@@ -145,7 +156,10 @@ const PaymentModal: React.FC<PaymentModalProps> = (
                 { required: true, message: "Vui lòng nhập ngày thanh toán" },
               ]}
             >
-              <CustomDatepicker style={{ width: "100%" }} />
+              <CustomDatepicker
+                style={{ width: "100%" }}
+                placeholder="dd/mm/yyyy"
+              />
             </Item>
           </Col>
 
@@ -162,18 +176,18 @@ const PaymentModal: React.FC<PaymentModalProps> = (
                 replace={(a: string) => replaceFormatString(a)}
                 min={0}
                 default={0}
-                placeholder=""
+                placeholder="Nhập số tiền cần thanh toán"
               />
             </Item>
           </Col>
           <Col xs={24} lg={12}>
             <Item name="reference" label="Số tham chiếu">
-              <Input />
+              <Input placeholder="Nhập số tham chiếu" disabled={disabledRef} maxLength={255} />
             </Item>
           </Col>
           <Col xs={24} lg={24}>
             <Item name="note" label="Ghi chú">
-              <Input.TextArea />
+              <Input maxLength={255} placeholder="Nhập ghi chú" />
             </Item>
           </Col>
         </Row>
