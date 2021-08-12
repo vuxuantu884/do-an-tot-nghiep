2db9e2020999efc418d4129473c45de5350572e4
manhnguyengg@gmail.com <manhnv@sunshinegroup.vn>
2021-08-12 10:40:51 +0700

    fix trang thai phu xu ly don hang (hoan thanh, dong goi, xuat kho) cua chi tiet don hang

--- src/screens/settings/printer/create/editor/index.tsx
+++ src/screens/settings/printer/create/editor/index.tsx
@@ -1,30 +1,66 @@
 import { PrintEditorModel } from "model/other/Print/print-model";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
+type ToolBarType = {
+  onClickRaw: () => void;
+};
 const Editor: React.FC<PrintEditorModel> = (props: PrintEditorModel) => {
   const { initialValue, onChange } = props;
   const [value, setValue] = useState(initialValue);
+  const [rawHtml, setRawHtml] = useState("");
+  const [showRaw, setShowRaw] = useState(false);
+  const toolbarRef = useRef(null);
   const handleOnChange = (value: string) => {
     setValue(value);
     onChange(value);
   };
+  const handleChangeRaw = (html: string) => {
+    setRawHtml(html);
+  };
+
+  const syncViews = (fromRaw: boolean) => {
+    if (fromRaw) {
+      setValue(rawHtml);
+    } else {
+      if (value) {
+        setRawHtml(value);
+      }
+    }
+  };
+
+  const handleClickShowRaw = () => {
+    setShowRaw(!showRaw);
+    syncViews(showRaw);
+  };
+  const CustomToolbar: React.FC<ToolBarType> = ({
+    onClickRaw,
+  }: ToolBarType) => (
+    <div id="toolbar">
+      <select className="ql-header">
+        <option value="1">Heading 1</option>
+        <option value="2">Heading 2</option>
+      </select>
+      <button className="ql-bold"></button>
+      <button className="ql-italic"></button>
+      <select className="ql-color">
+        <option value="red"></option>
+        <option value="green"></option>
+        <option value="blue"></option>
+        <option value="orange"></option>
+        <option value="violet"></option>
+        <option value="#d0d1d2"></option>
+      </select>
+      <button onClick={onClickRaw}>Raw</button>
+    </div>
+  );
   const editorConfig = {
     modules: {
-      toolbar: [
-        [{ header: "1" }, { header: "2" }, { font: [] }],
-        [{ size: [] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [
-          { list: "ordered" },
-          { list: "bullet" },
-          { indent: "-1" },
-          { indent: "+1" },
-        ],
-        ["link", "image", "video"],
-        ["clean"],
-      ],
+      toolbar: {
+        container: "#toolbar",
+        handlers: {},
+      },
       clipboard: {
         // toggle to add extra line breaks when pasting HTML:
         matchVisual: false,
@@ -44,18 +80,25 @@ const Editor: React.FC<PrintEditorModel> = (props: PrintEditorModel) => {
       "indent",
       "link",
       "image",
-      "video",
+      "color",
     ],
     theme: "snow",
   };
+
   return (
     <div>
+      <CustomToolbar onClickRaw={handleClickShowRaw} />
       <ReactQuill
         theme={editorConfig.theme}
         value={value}
         onChange={handleOnChange}
         modules={editorConfig.modules}
         formats={editorConfig.formats}
+      />
+      <textarea
+        className={"raw-editor"}
+        onChange={(e) => handleChangeRaw(e.target.value)}
+        value={rawHtml}
       />
     </div>
   );
--- src/screens/settings/printer/create/preview/index.tsx
+++ src/screens/settings/printer/create/preview/index.tsx
@@ -4,10 +4,28 @@ import "react-quill/dist/quill.snow.css";
 
 const Preview: React.FC<PrintPreviewModel> = (props: PrintPreviewModel) => {
   const { htmlContent } = props;
+  const FAKE_WORDS = [
+    {
+      symbol: "{ten_cong_ty}",
+      value: "YODY",
+    },
+    {
+      symbol: "{dia_chi_cong_ty}",
+      value: "Hải dương",
+    },
+  ];
+  const renderHtml = (htmlContent: string) => {
+    let result = "";
+    FAKE_WORDS.forEach((singleWord) => {
+      result = htmlContent.replaceAll(singleWord.symbol, singleWord.value);
+    });
+    console.log("result", result);
+    return result;
+  };
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: htmlContent,
+        __html: renderHtml(htmlContent),
       }}
     ></div>
   );
