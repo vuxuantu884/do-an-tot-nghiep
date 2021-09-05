22348f8cae5a8fec89268f45e1ca57e5d3b3749a
manhnguyengg@gmail.com <manhnv@sunshinegroup.vn>
2021-09-05 22:58:45 +0700

    update action log khi hủy

--- src/service/order/order.service.ts
+++ src/service/order/order.service.ts
@@ -100,7 +100,6 @@ export const updateShipment = (
   request: UpdateLineFulFillment
 ): Promise<BaseResponse<OrderResponse>> => {
   let link = `${ApiConfig.ORDER}/orders/${request.order_id}/shipment`;
-  console.log("request", request);
   return BaseAxios.put(link, request);
 };
 
