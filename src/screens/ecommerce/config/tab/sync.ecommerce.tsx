a2fb4ec94d2e8fffa0e1315bb9c3af306ac43671
bachnt <ngbach10@gmail.com>
2021-09-08 09:00:59 +0700

    fixbug UI emmmerce

--- src/screens/ecommerce/config/tab/sync.ecommerce.tsx
+++ src/screens/ecommerce/config/tab/sync.ecommerce.tsx
@@ -1,13 +1,13 @@
 import CustomTable from "component/table/CustomTable";
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { inventoryGetListAction } from "domain/actions/inventory/inventory.action";
-import { getQueryParams, useQuery } from "utils/useQuery";
-import { InventoryQuery, InventoryResponse } from "model/inventory";
-import { PageResponse } from "model/base/base-metadata.response";
-import { useHistory } from "react-router-dom";
-import { generateQuery } from "utils/AppUtils";
-import UrlConfig from "config/url.config";
+import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { inventoryGetListAction } from "domain/actions/inventory/inventory.action";
+// import { getQueryParams, useQuery } from "utils/useQuery";
+// import { InventoryQuery, InventoryResponse } from "model/inventory";
+// import { PageResponse } from "model/base/base-metadata.response";
+// import { useHistory } from "react-router-dom";
+// import { generateQuery } from "utils/AppUtils";
+// import UrlConfig from "config/url.config";
 import actionColumn from "../actions/action.column";
 import { StyledHeader } from "./styles";
 import { Button } from "antd";
@@ -16,55 +16,55 @@ import shopeeIcon from "assets/icon/shopee.png";
 import tikiIcon from "assets/icon/tiki.png";
 
 const SyncEcommerce: React.FC<any> = (props: any) => {
-  const history = useHistory();
-  const query = useQuery();
-  const dispatch = useDispatch();
-  let initQuery: InventoryQuery = {};
+  // const history = useHistory();
+  // const query = useQuery();
+  // // const dispatch = useDispatch();
+  // let initQuery: InventoryQuery = {};
 
-  let dataQuery: InventoryQuery = {
-    ...initQuery,
-    ...getQueryParams(query),
-  };
-  let [params, setPrams] = useState<InventoryQuery>(dataQuery);
-  const [data, setData] = useState<PageResponse<InventoryResponse>>({
-    metadata: {
-      limit: 30,
-      page: 1,
-      total: 0,
-    },
-    items: [],
-  });
-  const onResult = useCallback(
-    (result: PageResponse<InventoryResponse> | false) => {
-      if (result) {
-        setData(result);
-      }
-    },
-    []
-  );
-  const onPageChange = useCallback(
-    (page, size) => {
-      params.page = page;
-      params.limit = size;
-      let queryParam = generateQuery(params);
-      setPrams({ ...params });
-      history.replace(
-        `${UrlConfig.INVENTORY}${history.location.hash}?${queryParam}`
-      );
-    },
-    [history, params]
-  );
-  useEffect(() => {
-    if (props.current === "1") {
-      dispatch(inventoryGetListAction(params, onResult));
-    }
-  }, [dispatch, onResult, params, props]);
+  // let dataQuery: InventoryQuery = {
+  //   ...initQuery,
+  //   ...getQueryParams(query),
+  // };
+  // let [params, setPrams] = useState<InventoryQuery>(dataQuery);
+  // const [data, setData] = useState<PageResponse<InventoryResponse>>({
+  //   metadata: {
+  //     limit: 30,
+  //     page: 1,
+  //     total: 0,
+  //   },
+  //   items: [],
+  // });
+  // const onResult = useCallback(
+  //   (result: PageResponse<InventoryResponse> | false) => {
+  //     if (result) {
+  //       setData(result);
+  //     }
+  //   },
+  //   []
+  // );
+  // const onPageChange = useCallback(
+  //   (page, size) => {
+  //     params.page = page;
+  //     params.limit = size;
+  //     let queryParam = generateQuery(params);
+  //     setPrams({ ...params });
+  //     history.replace(
+  //       `${UrlConfig.INVENTORY}${history.location.hash}?${queryParam}`
+  //     );
+  //   },
+  //   [history, params]
+  // );
+  // useEffect(() => {
+  //   if (props.current === "1") {
+  //     dispatch(inventoryGetListAction(params, onResult));
+  //   }
+  // }, [dispatch, onResult, params, props]);
 
   //mock data
   const handleEdit = () => {};
   const handleDisconnect = () => {};
 
-  const [columns, setColumn] = useState<any>([
+  const [columns] = useState<any>([
     { title: "STT", visible: true, dataIndex: "index" },
     { title: "Sàn TMĐT", visible: true, dataIndex: "img" },
     { title: "Shop ID | Tên shop", visible: true, dataIndex: "shop_id" },
@@ -77,7 +77,7 @@ const SyncEcommerce: React.FC<any> = (props: any) => {
     actionColumn(handleEdit, handleDisconnect),
   ]);
 
-  const [dataMock, setdataMock] = useState<any>([
+  const [dataMock] = useState<any>([
     {
       img: "Link Img",
       shop_id: "YD6969",
@@ -93,11 +93,6 @@ const SyncEcommerce: React.FC<any> = (props: any) => {
     <div className="padding-20">
       <StyledHeader>
         <Button>
-          {/* <div>
-            <img src={shopeeIcon} alt="shopee" />
-            <img src={tikiIcon} alt="tiki" />
-            <img src={lazadaIcon} alt="lazada" />
-          </div> */}
           Tất cả gian hàng
         </Button>
         <Button>
@@ -122,14 +117,14 @@ const SyncEcommerce: React.FC<any> = (props: any) => {
       <CustomTable
         columns={columns}
         dataSource={dataMock}
-        pagination={{
-          pageSize: data.metadata.limit,
-          total: data.metadata.total,
-          current: data.metadata.page,
-          showSizeChanger: true,
-          onChange: onPageChange,
-          onShowSizeChange: onPageChange,
-        }}
+        // pagination={{
+        //   pageSize: data.metadata.limit,
+        //   total: data.metadata.total,
+        //   current: data.metadata.page,
+        //   showSizeChanger: true,
+        //   onChange: onPageChange,
+        //   onShowSizeChange: onPageChange,
+        // }}
         rowKey={(data) => data.id}
       />
     </div>
