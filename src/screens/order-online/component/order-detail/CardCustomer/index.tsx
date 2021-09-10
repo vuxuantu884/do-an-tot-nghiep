62f5ca3390520dd19797820a5475e714ba06186e
longchin <lvlong96k@gmail.com>
2021-09-10 12:06:18 +0700

    thêm inner style overlay

--- src/screens/order-online/component/order-detail/CardCustomer/index.tsx
+++ src/screens/order-online/component/order-detail/CardCustomer/index.tsx
@@ -713,6 +713,7 @@ const CustomerCard: React.FC<CustomerCardProps> = (
                       <Row>
                         <Popover
                           placement="bottomLeft"
+                          overlayStyle={{zIndex: 17}}
                           title={
                             <Row
                               justify="space-between"
