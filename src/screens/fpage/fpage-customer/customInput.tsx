d28cf0c2187250b7bc39d2aa45320d8f865dc075
bachnt <ngbach10@gmail.com>
2021-09-22 08:57:34 +0700

    Fix ecommerce

--- src/screens/fpage/fpage-customer/customInput.tsx
+++ src/screens/fpage/fpage-customer/customInput.tsx
@@ -2,19 +2,27 @@ import React, { useCallback, useState } from "react";
 import { Form, Input } from "antd";
 
 type CustomInputProps = {
-name: string;
-label: any;
-form?: any;
-message?: string;
-placeholder?: string;
-maxLength?: number
-isRequired?: boolean;
-suffix?: string;
-}
+  name: string;
+  label: any;
+  form?: any;
+  message?: string;
+  placeholder?: string;
+  maxLength?: number;
+  isRequired?: boolean;
+  suffix?: string;
+};
 
 function CustomInput(props: CustomInputProps) {
-  const { name, label, form, message, placeholder, maxLength, isRequired, suffix } =
-    props;
+  const {
+    name,
+    label,
+    form,
+    message,
+    placeholder,
+    maxLength,
+    isRequired,
+    suffix,
+  } = props;
   const [value, setValue] = useState<string>("");
 
   const handleChange = useCallback((v: any) => {
@@ -27,20 +35,17 @@ function CustomInput(props: CustomInputProps) {
   };
 
   React.useEffect(() => {
-    if(value) form.setFieldsValue({ [name]: value });
+    if (value) form.setFieldsValue({ [name]: value });
   }, [value, handleChange, form, name]);
 
   return (
     <Form.Item
       name={name}
       label={<div>{label}</div>}
-      rules={[
-        { required: isRequired, message: `${message}` },
-        
-      ]}
+      rules={[{ required: isRequired, message: `${message}` }]}
     >
       <Input
-        suffix={suffix? <span>{suffix}</span> : null}
+        suffix={suffix ? <span>{suffix}</span> : null}
         maxLength={maxLength}
         placeholder={`${placeholder}`}
         onBlur={(value) => handleBlur(value.target.value)}
