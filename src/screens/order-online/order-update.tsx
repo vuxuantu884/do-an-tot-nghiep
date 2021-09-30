18b5e84e0b1fbbc35fdd5505936de47ca9443028
longchin <lvlong96k@gmail.com>
2021-09-30 15:28:00 +0700

    fix limit_order_percent default null ==100%

--- src/screens/order-online/order-update.tsx
+++ src/screens/order-online/order-update.tsx
@@ -791,7 +791,7 @@ export default function Order(props: PropType) {
       let limitAmountPointFocus = !rank
         ? 0
         : !rank.limit_order_percent
-        ? 0
+        ? totalAmountPayable
         : (rank.limit_order_percent * totalAmountPayable) / 100;
       //limitAmountPointFocus= Math.floor(limitAmountPointFocus/1000);//số điểm tiêu tối đa cho phép
       limitAmountPointFocus = Math.round(limitAmountPointFocus / 1000); //số điểm tiêu tối đa cho phép
