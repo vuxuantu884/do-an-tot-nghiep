c98ca60b2b594ba4afb9731d0c52ca7ec08fa20d
manhnguyengg@gmail.com <manhnv@sunshinegroup.vn>
2021-10-07 15:22:03 +0700

    fix bugs

--- src/screens/order-online/order.screen.tsx
+++ src/screens/order-online/order.screen.tsx
@@ -602,7 +602,6 @@ export default function Order() {
               );
             }
             if (response) {
-              console.log("response333", response);
               let giftResponse = response.items.filter((item) => {
                 return item.type === Type.GIFT;
               });
@@ -701,6 +700,7 @@ export default function Order() {
               setIsLoadForm(true);
               if (
                 response.fulfillments &&
+                response.fulfillments.length > 0 &&
                 response.fulfillments[0].shipment?.cod
               ) {
                 setPaymentMethod(PaymentMethodOption.COD);
