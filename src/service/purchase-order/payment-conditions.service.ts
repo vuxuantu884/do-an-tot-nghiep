9c84e2370504004d5ba565980276bf999b0e5f38
anhnguyen <anhdev97@gmail.com>
2021-10-12 13:02:23 +0700

    apply all

--- src/service/purchase-order/payment-conditions.service.ts
+++ src/service/purchase-order/payment-conditions.service.ts
@@ -1,5 +1,5 @@
 
-import BaseAxios from "base/base-po.axios";
+import BaseAxios from "base/base.axios";
 import BaseResponse from "base/base.response";
 import { ApiConfig } from "config/api.config";
 import { PoPaymentConditions } from "model/purchase-order/payment-conditions.model";
