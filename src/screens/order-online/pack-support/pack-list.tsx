80672fa48828630d15ee8e7d744d3a46505a164f
longchin <lvlong96k@gmail.com>
2021-10-25 09:54:45 +0700

    fix  đóng gói

--- src/screens/order-online/pack-support/pack-list.tsx
+++ src/screens/order-online/pack-support/pack-list.tsx
@@ -1,51 +1,16 @@
 import { Card, Table } from "antd";
 import { ICustomTableColumType } from "component/table/CustomTable";
-import { PageResponse } from "model/base/base-metadata.response";
-import { useEffect, useState } from "react";
 import emptyProduct from "assets/icon/empty_products.svg";
-import { setPackInfo } from "utils/LocalStorageUtils";
+import { useEffect, useState } from "react";
 
 function PackList(props: any) {
-  const { data, onPageChange, queryParams } = props;
-  
-
-  // const [dataCustom, setDataCustom] = useState<PageResponse<any>>({
-  //   metadata: {
-  //     limit: 1,
-  //     page: 1,
-  //     total: 0,
-  //   },
-  //   items: [],
-  // });
-
-  // useEffect(() => {
-  //   if (data.items.length > 0 && queryParams) {
-  //     let datas: PageResponse<any> = {
-  //       metadata: {
-  //         limit: 1,
-  //         page: 1,
-  //         total: 0,
-  //       },
-  //       items: [],
-  //     };
-
-  //     //let totalPage = Math.ceil(data.metadata.total / queryParams.limit);
-  //     let index = (queryParams.page - 1) * queryParams.limit;
-
-  //     for (let i = 0; i < queryParams.limit; i++) {
-  //       let stt = index + i;
-  //       if (stt <= data.items.length - 1) {
-  //         datas.items.push(data.items[stt]);
-  //       }
-  //     }
-
-  //     datas.metadata.total = Number(data.items.length);
-  //     datas.metadata.page = queryParams.page;
-  //     datas.metadata.limit = queryParams.limit;
-
-  //     setDataCustom(datas);
-  //   }
-  // }, [data, queryParams]);
+  const { data } = props;
+  const [dataPack, setDataPack]=useState([])
+  useEffect(() => {
+    if (data.items.length > 0) {
+      setDataPack(data.items);
+    }
+  }, [data])
 
   const columnsOrderPack: Array<ICustomTableColumType<any>> = [
     {
@@ -102,7 +67,7 @@ function PackList(props: any) {
       <div>
       <Table 
         columns={columnsOrderPack}
-        dataSource={data.items} 
+        dataSource={dataPack} 
         locale={{
           emptyText: (
             <div className="sale_order_empty_product">
@@ -113,29 +78,6 @@ function PackList(props: any) {
         }}
         className="ecommerce-order-list"
       />
-        {/* <Table
-          locale={{
-            emptyText: (
-              <div className="sale_order_empty_product">
-                <img src={emptyProduct} alt="empty product"></img>
-                <p>Không có dữ liệu!</p>
-              </div>
-            ),
-          }}
-          pagination={{
-            //pageSize: 1,
-            //total: dataCustom?.metadata.total,
-            //current: dataCustom?.metadata.page,
-            showSizeChanger: true,
-            onChange: onPageChange,
-            onShowSizeChange: onPageChange,
-          }}
-          dataSource={dataCustom}
-          columns={columnsOrderPack}
-          rowKey={(item: any) => item.id}
-          className="ecommerce-order-list"
-          key={Math.random()}
-        /> */}
       </div>
     </Card>
   );
