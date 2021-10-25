5cee8ca11a8157223ae0b454b580d4a464bd351d
tungdv <dovantunghy186@gmail.com>
2021-10-25 15:48:46 +0700

    commit

--- src/screens/order-online/component/order-detail/CardShipment/index.tsx
+++ src/screens/order-online/component/order-detail/CardShipment/index.tsx
@@ -133,8 +133,11 @@ const CardShipment: React.FC<CardShipmentProps> = (props: CardShipmentProps) => 
     // if (value !== ShipmentMethodOption.DELIVER_PARTNER) {
     //   // onPayments([]);
     // }
-    if (value === ShipmentMethodOption.DELIVER_LATER || value === ShipmentMethodOption.PICK_AT_STORE) {
-      setShippingFeeInformedCustomer(0)
+    if (
+      value === ShipmentMethodOption.DELIVER_LATER ||
+      value === ShipmentMethodOption.PICK_AT_STORE
+    ) {
+      setShippingFeeInformedCustomer(0);
     }
   };
 
@@ -157,7 +160,7 @@ const CardShipment: React.FC<CardShipmentProps> = (props: CardShipmentProps) => 
     setFee(fee);
     setHvcName && setHvcName(name);
     setHvcCode && setHvcCode(code);
-    setServiceName(serviceName)
+    setServiceName(serviceName);
   };
 
   useLayoutEffect(() => {
@@ -216,8 +219,8 @@ const CardShipment: React.FC<CardShipmentProps> = (props: CardShipmentProps) => 
     if (
       customerInfo &&
       storeDetail &&
-      (getShippingAddressDefault(customerInfo)?.city_id ||
-        getShippingAddressDefault(customerInfo)?.district_id) &&
+      getShippingAddressDefault(customerInfo)?.city_id &&
+      getShippingAddressDefault(customerInfo)?.district_id &&
       getShippingAddressDefault(customerInfo)?.ward_id &&
       getShippingAddressDefault(customerInfo)?.full_address
     ) {
