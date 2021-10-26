c29eb2e0879a30a9781a30cd67593cf970938ad8
Thai <nguyenvanthai3443@gmail.com>
2021-10-26 21:36:52 +0700

    [ES-404] Update Popup xác nhận đơn tải về

--- src/screens/ecommerce/orders/component/ResultDownloadOrderDataModal.tsx
+++ src/screens/ecommerce/orders/component/ResultDownloadOrderDataModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal } from "antd";
 
 import checkCircleIcon from "assets/icon/check-circle.svg";
+import checkCircleBlueIcon from "assets/icon/check-circle-blue.svg";
 import errorIcon from "assets/icon/error.svg";
 
 
@@ -9,14 +10,14 @@ type ResultDownloadOrderDataModalType = {
   visible: boolean;
   onOk: () => void;
   onCancel: () => void;
-  data: any;
+  downloadOrderData: any;
 };
 
 
 const ResultDownloadOrderDataModal: React.FC<ResultDownloadOrderDataModalType> = (
   props: ResultDownloadOrderDataModalType
 ) => {
-  const { visible, onOk, onCancel, data } = props;
+  const { visible, onOk, onCancel, downloadOrderData } = props;
   
 
   return (
@@ -24,20 +25,27 @@ const ResultDownloadOrderDataModal: React.FC<ResultDownloadOrderDataModalType> =
       width="600px"
       className=""
       visible={visible}
-      title={"Có " + data?.total + " đơn hàng được cập nhật thành công"}
+      title={"Có " + downloadOrderData?.total + " đơn hàng được cập nhật thành công"}
       okText="Đóng"
       onOk={onOk}
       onCancel={onCancel}
       cancelButtonProps={{ style: { display: 'none' } }}
+      maskClosable={false}
     >
       <div>
         <div>
           <img src={checkCircleIcon} style={{ marginRight: 5 }} alt="" />
-          <span>Có <p style={{color: "green", display: "inline-block"}}>{data?.create_order_count}</p> đơn hàng được tải mới thành công</span>
+          <span>Có <p style={{color: "green", display: "inline-block"}}>{downloadOrderData?.create_total}</p> đơn hàng được tải mới thành công</span>
         </div>
+        
+        <div>
+          <img src={checkCircleBlueIcon} style={{ marginRight: 5 }} alt="" />
+          <span>Có <p style={{color: "#2A2A86", display: "inline-block"}}>{downloadOrderData?.update_total}</p> đơn hàng được cập nhật thành công</span>
+        </div>
+
         <div>
           <img src={errorIcon} style={{ marginRight: 5 }} alt="" />
-          <span>Có <p style={{color: "red", display: "inline-block"}}>{data?.update_order_count}</p> đơn hàng thất bại</span>
+          <span>Có <p style={{color: "red", display: "inline-block"}}>{downloadOrderData?.error_total}</p> đơn hàng thất bại</span>
         </div>
       </div>
     </Modal>
