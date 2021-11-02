00241a32c9f5c3ed45eca9bbccbb63e562308d4a
FE - Minh Nhat <nhatnm28@gmail.com>
2021-11-02 11:43:04 +0700

    update ui product list screen

---
+++ src/component/tag/tag-status.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const TagStatusStyle = styled.div`
+  .ant-tag {
+    padding: 2px 8px;
+    border-radius: 20px;
+  }
+  .outline {
+    border: none;
+    background-color: white !important;
+  }
+`;
