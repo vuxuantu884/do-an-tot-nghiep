0dc6da921c93d60903c613fb358842fe601ac2c7
longchin <lvlong96k@gmail.com>
2021-11-09 17:08:18 +0700

    them storeid cho tim kiem san pham tai SO

--- src/component/order/OrderCreateProduct/index.tsx
+++ src/component/order/OrderCreateProduct/index.tsx
@@ -927,9 +927,10 @@ function OrderCreateProduct(props: PropType) {
       }
 
       initQueryVariant.info = value;
+      initQueryVariant.store_ids=form?.getFieldValue(["store_id"]);
+      console.log("initQueryVariant",initQueryVariant)
       if (value.trim()) {
         (async () => {
-          // console.log('setSearchProducts true');
           setSearchProducts(true);
           try {
             await dispatch(
@@ -1192,6 +1193,15 @@ function OrderCreateProduct(props: PropType) {
                   if (value) {
                     setStoreId(value);
                     setIsShowProductSearch(true);
+                    setKeySearchVariant("");
+                    setResultSearchVariant({
+                      metadata: {
+                        limit: 0,
+                        page: 1,
+                        total: 0,
+                      },
+                      items: [],
+                    })
                   } else {
                     setIsShowProductSearch(false);
                   }
