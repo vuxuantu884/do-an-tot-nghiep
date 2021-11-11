2b9ee28c623fb4422c127feada5f09d7c8e089a6
giangyang <yang@travala.com>
2021-11-12 05:03:13 +0700

    resolve conflict with develop

--- src/screens/order-online/component/order-detail/CardProduct/index.tsx
+++ src/screens/order-online/component/order-detail/CardProduct/index.tsx
@@ -14,7 +14,9 @@ import {
   Table,
   Tooltip
 } from "antd";
+// import _ from "lodash";
 import { RefSelectProps } from "antd/lib/select";
+import _ from "lodash";
 import emptyProduct from "assets/icon/empty_products.svg";
 import giftIcon from "assets/icon/gift.svg";
 import imgDefault from "assets/icon/img-default.svg";
