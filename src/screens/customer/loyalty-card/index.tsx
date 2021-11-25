e8f0c107f1b3004aadc3dae3a1413d43e81f77c4
Thai <nguyenvanthai3443@gmail.com>
2021-11-25 12:05:05 +0700

    [ES-714-1][Update] Loy | Refactor CODE FE

--- src/screens/customer/loyalty-card/index.tsx
+++ src/screens/customer/loyalty-card/index.tsx
@@ -7,6 +7,7 @@ import useAuthorization from 'hook/useAuthorization';
 import { useState } from 'react';
 import LoyaltyCards from './list';
 import LoyaltyCardRelease from 'screens/customer/loyalty-card/release';
+import { StyledCustomerCard } from 'screens/customer/loyalty-card/StyledCustomerCard';
 
 
 const { TabPane } = Tabs;
@@ -21,41 +22,43 @@ const LoyaltyCardPage = () => {
 
   const [activeTab, setActiveTab] = useState<string>('1')
   return (
-    <ContentContainer
-      title="Thẻ khách hàng"
-      breadcrumb={[
-        {
-          name: "Tổng quan",
-          path: UrlConfig.HOME,
-        },
-        {
-          name: "Thẻ khách hàng"
-        }
-      ]}
-      extra={
-        <>
+    <StyledCustomerCard>
+      <ContentContainer
+        title="Thẻ khách hàng"
+        breadcrumb={[
           {
-            activeTab === '1' && allowCreateCardRelease && (
-              <ButtonCreate
-                path={`${UrlConfig.CUSTOMER}/cards/upload`}
-                child="Thêm mới"
-              />
-            )
+            name: "Tổng quan",
+            path: UrlConfig.HOME,
+          },
+          {
+            name: "Thẻ khách hàng"
           }
-        </>
-      }
-    >
-      <Card>
-        <Tabs defaultActiveKey="1" onChange={(activeKey: string) => setActiveTab(activeKey)}>
-          <TabPane tab="Đợt phát hành" key="1">
-            <LoyaltyCardRelease />
-          </TabPane>
-          <TabPane tab="Thẻ phát hành" key="2">
-            <LoyaltyCards />
-          </TabPane>
-        </Tabs>
-      </Card>
-    </ContentContainer>
+        ]}
+        extra={
+          <>
+            {
+              activeTab === '1' && allowCreateCardRelease && (
+                <ButtonCreate
+                  path={`${UrlConfig.CUSTOMER}/cards/upload`}
+                  child="Thêm mới"
+                />
+              )
+            }
+          </>
+        }
+      >
+        <Card className="customer-card">
+          <Tabs defaultActiveKey="1" onChange={(activeKey: string) => setActiveTab(activeKey)}>
+            <TabPane tab="Đợt phát hành" key="1">
+              <LoyaltyCardRelease />
+            </TabPane>
+            <TabPane tab="Thẻ phát hành" key="2">
+              <LoyaltyCards />
+            </TabPane>
+          </Tabs>
+        </Card>
+      </ContentContainer>
+    </StyledCustomerCard>
   )
 }
 
