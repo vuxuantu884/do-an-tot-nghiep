dff3511e657e99555b9420d33440f10feb9d08cb
manhnguyengg@gmail.com <manhnv@sunshinegroup.vn>
2021-11-26 18:44:44 +0700

    fix layout chi tiết đơn hàng

--- src/screens/order-online/component/update-shipment-card.tsx
+++ src/screens/order-online/component/update-shipment-card.tsx
@@ -946,7 +946,7 @@ const UpdateShipmentCard: React.FC<UpdateShipmentCardProps> = (
                   ]}
                   // onChange={(e) => console.log(e[0])}
                   expandIcon={({isActive}) => (
-                    <div className="saleorder-header-arrow">
+                    <div className="saleorder-header-arrow 2">
                       <img
                         alt=""
                         src={doubleArrow}
@@ -970,8 +970,8 @@ const UpdateShipmentCard: React.FC<UpdateShipmentCardProps> = (
                     }
                     showArrow={true}
                     header={
-                      <div className="saleorder-header-content">
-                        <div className="saleorder-header-content__info">
+                      <div className="saleorder-header-content" style={{display: "flex", width: "100%", padding: 0}}>
+                        <div className="saleorder-header-content__info" style={{display: "flex", width: "100%"}}>
                           <span
                             className="text-field"
                             style={{
@@ -1009,7 +1009,7 @@ const UpdateShipmentCard: React.FC<UpdateShipmentCardProps> = (
                             />}
                         </div>
 
-                        <div className="saleorder-header-content__date">
+                        <div className="saleorder-header-content__date" style={{display: "flex", width: "100%", alignItems: "center"}}>
                           {(fulfillment.status === FulFillmentStatus.CANCELLED ||
                             fulfillment.status === FulFillmentStatus.RETURNING ||
                             fulfillment.status === FulFillmentStatus.RETURNED) ?
@@ -1286,7 +1286,7 @@ const UpdateShipmentCard: React.FC<UpdateShipmentCardProps> = (
                               <Panel
                                 header={
                                   <Row>
-                                    <Col style={{alignItems: "center"}}>
+                                    <Col style={{display: "flex", width: "100%", alignItems: "center"}}>
                                       <span
                                         style={{
                                           marginRight: "10px",
