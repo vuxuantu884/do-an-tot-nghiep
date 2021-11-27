13ce3dbb2df3c322badf3409fa32c3945deb2a07
manhnguyengg@gmail.com <manhnguyengg@gmail.com>
2021-11-27 09:22:04 +0700

    fix lại model đổi trả

--- src/service/promotion/discount/discount.service.ts
+++ src/service/promotion/discount/discount.service.ts
@@ -5,7 +5,8 @@ import {generateQuery} from "../../../utils/AppUtils";
 import BaseAxios from "../../../base/base.axios";
 import {ApiConfig} from "../../../config/api.config";
 import {DiscountResponse} from "../../../model/response/promotion/discount/list-discount.response";
-import { CouponRequestModel } from "model/request/promotion.request";
+import { CouponRequestModel, DiscountRequestModel } from "model/request/promotion.request";
+import { ApplyCouponResponseModel } from "model/response/order/promotion.response";
 
 const END_POINT = "/price-rules";
 
@@ -69,6 +70,10 @@ export const applyDiscount = (items: Array<any>, orderInfo:any, orderId?: number
 }
 
 
-export const applyCouponService = (queryParams: CouponRequestModel): Promise<any> => {
+export const applyCouponService = (queryParams: CouponRequestModel): Promise<BaseResponse<ApplyCouponResponseModel>> => {
+  return BaseAxios.post(`${ApiConfig.PROMOTION}${END_POINT}/apply`, queryParams)
+};
+
+export const applyDiscountService = (queryParams: DiscountRequestModel): Promise<BaseResponse<ApplyCouponResponseModel>> => {
   return BaseAxios.post(`${ApiConfig.PROMOTION}${END_POINT}/apply`, queryParams)
 };
