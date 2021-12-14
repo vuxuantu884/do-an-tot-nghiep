2e85e46d150d92028ba485c941b6e440a35fb021
Nhật Nguyễn <nhatnm28@gmail.com>
2021-12-14 16:55:19 +0000

    Feature/pro 255 add select parent product

--- src/screens/purchase-order/component/product-item.tsx
+++ src/screens/purchase-order/component/product-item.tsx
@@ -4,18 +4,18 @@ import { AppConfig } from "config/app.config";
 import { VariantResponse } from "model/product/product.model";
 import { formatCurrency, Products } from "utils/AppUtils";
 
-type ProductItemProps = {
-  data: VariantResponse
+export type ProductItemProps = {
+  data: VariantResponse;
   showCheckBox?: boolean
   checked?: boolean
   onChange?: (checked: boolean) => void
-  isTransfer ?: boolean
+  isTransfer?: boolean
 }
 
 const ProductItem: React.FC<ProductItemProps> = (props: ProductItemProps) => {
-  const {data, showCheckBox, checked, isTransfer} = props;
+  const { data, showCheckBox, checked, isTransfer } = props;
   const avatar = Products.findAvatar(data.variant_images);
-  const price_data = Products.findPrice(data.variant_prices,  AppConfig.currency);
+  const price_data = Products.findPrice(data.variant_prices, AppConfig.currency);
   return (
     <div className="product-item">
       {
@@ -34,9 +34,9 @@ const ProductItem: React.FC<ProductItemProps> = (props: ProductItemProps) => {
         <div className="product-item-info-right">
           {
             isTransfer ? (
-              <span className="product-item-price">{price_data && price_data.retail_price ?  formatCurrency(price_data.retail_price) : 0} <span className="currency">₫</span></span>
+              <span className="product-item-price">{price_data && price_data.retail_price ? formatCurrency(price_data.retail_price) : 0} <span className="currency">₫</span></span>
             ) : (
-              <span className="product-item-price">{price_data && price_data.import_price ?  formatCurrency(price_data.import_price) : 0} <span className="currency">₫</span></span>
+              <span className="product-item-price">{price_data && price_data.import_price ? formatCurrency(price_data.import_price) : 0} <span className="currency">₫</span></span>
             )
           }
           <span className="product-item-inventory">Số lượng tồn: <span className="value">{data.on_hand ? data.on_hand : 0}</span></span>
--- src/utils/PromotionUtils.ts
+++ src/utils/PromotionUtils.ts
@@ -1,9 +1,16 @@
 import {FormInstance} from "antd";
+import {YodyAction} from "base/base.action";
+import {searchProductWrapperRequestAction} from "domain/actions/product/products.action";
+import _ from "lodash";
+import {PageResponse} from "model/base/base-metadata.response";
+import {ProductResponse, VariantResponse} from "model/product/product.model";
 import {
   DiscountFormModel,
   VariantEntitlementsResponse,
 } from "model/promotion/discount.create.model";
+import {Dispatch} from "redux";
 import {formatCurrency} from "./AppUtils";
+import {showError} from "./ToastUtils";
 
 export const shareDiscount = (
   oldDiscountList: Array<DiscountFormModel>,
@@ -22,7 +29,7 @@ export const shareDiscount = (
       ignoreVariantMap.set(child.variant_id, child);
     });
   });
-
+  
   newVariantList.forEach((item, index) => {
     let discount = discountMap.get(item.discount_value);
     console.log("parent", typeof discount?.variants);
@@ -38,6 +45,7 @@ export const shareDiscount = (
             ? "FIXED_AMOUNT"
             : item.discount_type,
       };
+
       oldDiscountList.push(discount);
       discountMap.set(item.discount_value, discount);
     } else if (discount && !ignoreVariantMap.get(item.variant_id)) {
@@ -47,8 +55,10 @@ export const shareDiscount = (
       ignoreVariantMap.set(item.variant_id, item);
     }
   });
+  
+  form.setFieldsValue({entitlements: oldDiscountList});
 
-  return oldDiscountList;
+  return oldDiscountList.map((item) => item.variants);
 };
 
 export function nonAccentVietnamese(str: string) {
@@ -134,4 +144,91 @@ export const getDayOptions = () => {
     days.push({key: `${i}`, value: `Ngày ${i}`});
   }
   return days;
+};
+
+export const handleSearchProduct = _.debounce(
+  (
+    dispatch: Dispatch<YodyAction>,
+    value: string,
+    onSetData: (data: Array<ProductResponse>) => void
+  ) => {
+    dispatch(
+      searchProductWrapperRequestAction(
+        {
+          status: "active",
+          limit: 200,
+          page: 1,
+          info: value.trim(),
+        },
+        (response: PageResponse<ProductResponse> | false) => {
+          if (response) {
+            onSetData(response.items);
+          }
+        }
+      )
+    );
+  },
+  500
+);
+
+/**
+ *
+ * @param value : selected product
+ * @param isVariant : is variant or parent product
+ * @param dataSearchVariant : result of search when typing in search variant
+ * @param selectedProductParentRef : ref of selected product parent
+ * @param setIsVisibleConfirmModal : set visible confirm modal
+ * @param selectedProduct : the products display in table
+ * @param setSelectedProduct : set selected product to display in table
+ * @returns none
+ */
+export const onSelectProduct = (
+  value: string,
+  isVariant: boolean,
+  dataSearchVariant: Array<VariantResponse>,
+  selectedProductParentRef: any,
+  setIsVisibleConfirmModal: (isVisible: boolean) => void,
+  selectedProduct: Array<any>,
+  setSelectedProduct: (selectedProduct: Array<any>) => void
+) => {
+  let selectedItem: any;
+  //check is variant or product
+  if (isVariant) {
+    selectedItem = dataSearchVariant.find((e) => e.id === Number(value));
+  } else {
+    selectedItem = JSON.parse(value);
+  }
+  /**
+   *  if selected item is not exist in selected product
+   */
+  const checkExist = selectedProduct.some((e) => e.id === selectedItem.id);
+  if (checkExist) {
+    showError("Sản phẩm đã được chọn!");
+    return;
+  }
+  /**
+   * Trường hợp trên ds đã có mã cha
+   * → tìm kiếm sp variant thì thông báo lỗi đã đưa mã cha vào ds
+   * → Nếu muốn thay đổi thì xóa mã cha
+   */
+  if (isVariant && selectedProduct.some((e) => selectedItem?.sku?.includes(e?.code))) {
+    showError("Sản phẩm đã được chọn mã cha");
+    return;
+  }
+
+  /**
+   * Trường hợp trên ds đã có mã variant
+   * → tìm kiếm sp  cha thì thông báo đã có mã variant vào ds
+   * → popup thông báo sẽ áp dụng các variant còn lại của mã này
+   * → Bấm XÁC NHẬN/ HỦY
+   */
+  if (!isVariant && selectedProduct.some((e) => e?.sku?.startsWith(selectedItem?.code))) {
+    selectedProductParentRef.current = selectedItem;
+    setIsVisibleConfirmModal(true);
+    return;
+  }
+
+  if (selectedItem) {
+    setSelectedProduct([selectedItem].concat(selectedProduct));
+  }
 };
