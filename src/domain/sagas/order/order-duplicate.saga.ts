e7f7461b69184299fc00e67cc5ff636bb788eb34
longchin <lvlong96k@gmail.com>
2021-12-16 11:45:54 +0700

    update đơn trùng

--- src/domain/sagas/order/order-duplicate.saga.ts
+++ src/domain/sagas/order/order-duplicate.saga.ts
@@ -1,7 +1,13 @@
-import { CustomerDuplicateModel } from './../../../model/order/duplicate.model';
-import { OrderType } from 'domain/types/order.type';
-import {getOrderDuplicateService, putOrderCancelService, putOrderMergeService} from "./../../../service/order/order-duplicate.service";
-import {call,takeLatest,put} from "@redux-saga/core/effects";
+import {OrderModel} from "model/order/order.model";
+import {CustomerDuplicateModel} from "./../../../model/order/duplicate.model";
+import {OrderType} from "domain/types/order.type";
+import {
+  getDetailOrderDuplicateService,
+  getOrderDuplicateService,
+  putOrderCancelService,
+  putOrderMergeService,
+} from "./../../../service/order/order-duplicate.service";
+import {call, takeLatest, put} from "@redux-saga/core/effects";
 import {YodyAction} from "base/base.action";
 import {showError} from "utils/ToastUtils";
 import BaseResponse from "base/base.response";
@@ -24,7 +30,7 @@ function* getOrderDuplicateSaga(action: YodyAction) {
         yield put(unauthorizedAction());
         break;
       default:
-          response.errors.forEach((e)=>showError(e));
+        response.errors.forEach((e) => showError(e));
         break;
     }
   } catch {
@@ -32,52 +38,74 @@ function* getOrderDuplicateSaga(action: YodyAction) {
   }
 }
 
-function* putOrderDuplicateMergeSaga(action:YodyAction){
-  let{origin_id,ids,setData}=action.payload;
-  try{
-    let response:BaseResponse<any>=yield call(putOrderMergeService,origin_id, ids);
-    switch(response.code)
-    {
+function* putOrderDuplicateMergeSaga(action: YodyAction) {
+  let {origin_id, ids, setData} = action.payload;
+  try {
+    let response: BaseResponse<OrderModel> = yield call(putOrderMergeService, origin_id, ids);
+    switch (response.code) {
       case HttpStatus.SUCCESS:
+        setData(response.data);
         break;
       case HttpStatus.UNAUTHORIZED:
         yield put(unauthorizedAction());
         break;
       default:
-        response.errors.forEach((e:any)=>showError(e));
+        response.errors.forEach((e: any) => showError(e));
         break;
     }
-  }
-  catch{
-    showError("Có lỗi xảy ra khi thực hiện thao tác gộp đơn trùng")
+  } catch {
+    showError("Có lỗi xảy ra khi thực hiện thao tác gộp đơn trùng");
   }
 }
 
-function* putOrderDuplicateCancelSaga(action:YodyAction)
-{
-  let{ids,setData}=action.payload;
-  try{
-    let response:BaseResponse<any>= yield call(putOrderCancelService,ids);
-    switch(response.code)
-    {
+function* putOrderDuplicateCancelSaga(action: YodyAction) {
+  let {ids, setData} = action.payload;
+  try {
+    let response: BaseResponse<any> = yield call(putOrderCancelService, ids);
+    switch (response.code) {
       case HttpStatus.SUCCESS:
         setData(true);
         break;
       case HttpStatus.UNAUTHORIZED:
+        setData(false);
         yield put(unauthorizedAction());
         break;
       default:
-        response.errors.forEach((e:any)=>showError(e));
+        setData(false);
+        response.errors.forEach((e: any) => showError(e));
         break;
     }
-  }
-  catch{
+  } catch {
     showError("Có lỗi xảy ra khi thực hiện thao tác hủy đơn trùng");
   }
 }
 
-export function* OrderDuplicateSaga(){
-    yield takeLatest(OrderType.GET_ORDER_DUPLICATE_LIST, getOrderDuplicateSaga);
-    yield takeLatest(OrderType.PUT_ORDER_DUPLICATE_MERGE,putOrderDuplicateMergeSaga);
-    yield takeLatest(OrderType.PUT_ORDER_DUPLICATE_CANCEL,putOrderDuplicateCancelSaga);
+function* getDetailOrderDuplicateSaga(action: YodyAction) {
+  let {query, setData} = action.payload;
+  try {
+    let response: BaseResponse<Array<OrderModel>> = yield call(
+      getDetailOrderDuplicateService,
+      query
+    );
+    switch (response.code) {
+      case HttpStatus.SUCCESS:
+        setData(response.data);
+        break;
+      case HttpStatus.UNAUTHORIZED:
+        yield put(unauthorizedAction());
+        break;
+      default:
+        response.errors.forEach((e) => showError(e));
+        break;
+    }
+  } catch (error) {
+    showError("Có lỗi khi lấy dữ liệu danh sách đơn hàng! Vui lòng thử lại sau!");
+  }
+}
+
+export function* OrderDuplicateSaga() {
+  yield takeLatest(OrderType.GET_ORDER_DUPLICATE_LIST, getOrderDuplicateSaga);
+  yield takeLatest(OrderType.PUT_ORDER_DUPLICATE_MERGE, putOrderDuplicateMergeSaga);
+  yield takeLatest(OrderType.PUT_ORDER_DUPLICATE_CANCEL, putOrderDuplicateCancelSaga);
+  yield takeLatest(OrderType.GET_DETAIL_ORDER_DUPLICATE, getDetailOrderDuplicateSaga);
 }
