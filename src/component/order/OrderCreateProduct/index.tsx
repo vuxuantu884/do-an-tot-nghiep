8ebab8dfb821db0f43434adfde943542a06f1726
manhnguyengg@gmail.com <manhnv@sunshinegroup.vn>
2021-12-25 08:57:55 +0700

    fix bug chiết khấu

--- src/component/order/OrderCreateProduct/index.tsx
+++ src/component/order/OrderCreateProduct/index.tsx
@@ -322,10 +322,7 @@ function OrderCreateProduct(props: PropType) {
 
 								if (splitLine || index === -1) {
 									_items.push(item);
-									calculateChangeMoney(
-										_items,
-										promotion
-									);
+									calculateChangeMoney(_items);
 								} else {
 									let variantItems = _items.filter((item) => item.variant_id === data.id);
 									let lastIndex = variantItems.length - 1;
@@ -333,10 +330,7 @@ function OrderCreateProduct(props: PropType) {
 									variantItems[lastIndex].line_amount_after_line_discount +=
 										variantItems[lastIndex].price -
 										variantItems[lastIndex].discount_items[0].amount;
-									calculateChangeMoney(
-										_items,
-										promotion
-									);
+									calculateChangeMoney(_items);
 								}
 
 								setItems(_items.reverse());
@@ -1412,6 +1406,8 @@ function OrderCreateProduct(props: PropType) {
 		}
 	};
 
+	console.log('promotion', promotion)
+
 	const onSearchVariantSelect = useCallback(
 		async (v, o) => {
 			if (!items) {
@@ -1428,10 +1424,7 @@ function OrderCreateProduct(props: PropType) {
 			if (r.id === newV && checkInventory(item) === true) {
 				if (splitLine || index === -1) {
 					_items.unshift(item);
-					calculateChangeMoney(
-						_items,
-						promotion
-					);
+					calculateChangeMoney(_items);
 				} else {
 					let variantItems = _items.filter((item) => item.variant_id === newV);
 					let firstIndex = 0;
@@ -1440,10 +1433,7 @@ function OrderCreateProduct(props: PropType) {
 						variantItems[firstIndex].price -
 						variantItems[firstIndex].discount_items[0]?.amount *
 						variantItems[firstIndex].quantity;
-					calculateChangeMoney(
-						_items,
-						promotion
-					);
+					calculateChangeMoney(_items);
 				}
 			}
 			if (isAutomaticDiscount && _items.length > 0) {
