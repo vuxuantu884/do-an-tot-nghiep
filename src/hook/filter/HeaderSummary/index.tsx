fb35aac4e5070f85f95ba35045c5a511fcc4d2aa
Được Nguyễn Cố <ncduoc2502@gmail.com>
2021-12-26 23:00:28 +0700

    add sort column inventories

---
+++ src/hook/filter/HeaderSummary/index.tsx
@@ -0,0 +1,26 @@
+import { CaretDownOutlined, CaretUpOutlined } from "@ant-design/icons";
+import { formatCurrency } from "utils/AppUtils";
+import { sortActionColor } from "utils/global-styles/variables";
+
+export const HeaderSummary = ( 
+  total: number | undefined, 
+  header: string,
+  field: string,
+  onSortASC?: (sortColumn: string)=> void,
+  onSortDESC?: (sortColumn: string)=> void )=>{
+    let SortComponent = () => <div style={{display: "grid", marginLeft: "10px"}}>
+                        <CaretUpOutlined style={{color: sortActionColor, fontSize: 12}} title="Sắp xếp tăng dần" size={5} onClick={()=>{onSortASC && onSortASC(field)}} />
+                        <CaretDownOutlined style={{color: sortActionColor, fontSize: 12}} title="Sắp xếp giảm dần" size={10} onClick={()=>{onSortDESC && onSortDESC(field)}} />
+                      </div>
+    let Component = () => <div style={{display: "inline-flex"}}>{header} <SortComponent/></div>;
+    if (total) { 
+      Component = () => (
+        <>
+          <div style={{display: "inline-flex"}}>{header} <SortComponent/></div>
+          <div>{`(${formatCurrency(total,".")})`}</div>
+        </>
+      );
+    }
+    return Component;
+}
+ 
