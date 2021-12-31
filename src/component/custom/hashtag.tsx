584e9199d1020ee4cf2cea2c022bc34f789866e3
Thai <nguyenvanthai3443@gmail.com>
2021-12-31 13:13:04 +0700

    [UT-1880][Update]trường tag khi xóa đi chưa cho nhập lại

--- src/component/custom/hashtag.tsx
+++ src/component/custom/hashtag.tsx
@@ -1,6 +1,7 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { StyledHashTag } from "component/custom/StyledCustomComponent";
-import { Input, Tag, Tooltip } from "antd";
+import { Input, Tooltip } from "antd";
+import xCloseBtn from "assets/icon/X_close.svg";
 
 type HashTagProps = {
   value?: string;
@@ -11,20 +12,23 @@ type HashTagProps = {
 const HashTag: React.FC<HashTagProps> = (props: HashTagProps) => {
   const [inputValue, setInputValue] = useState<string>("");
   const [border, setBorder] = useState<boolean>(false);
+  const [tagList, setTagList] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    let newTags: Array<any> = [];
+    if (props.value) {
+      newTags = props.value.split(",");
+    }
+    setTagList(newTags);
+  }, [props.value]);
 
   const handleChange = (e: any) => {
     if (e.target.value === ",") return;
     setInputValue(e.target.value);
   };
 
-  const listTag = useMemo(() => {
-    if (props.value) {
-      return props.value.split(",");
-    }
-    return [];
-  }, [props.value]);
   const handleKeyDown = (e: any) => {
-    let _tags = [...listTag];
+    let _tags = [...tagList];
     if (
       (e.which === 13 && inputValue !== "") ||
       (e.which === 188 && inputValue !== "")
@@ -39,7 +43,7 @@ const HashTag: React.FC<HashTagProps> = (props: HashTagProps) => {
   };
 
   const handleDelete = (index: number) => {
-    let _tags = [...listTag];
+    let _tags = [...tagList];
     _tags.splice(index, 1);
     props.onChange && props.onChange(_tags.join(","));
   };
@@ -57,23 +61,18 @@ const HashTag: React.FC<HashTagProps> = (props: HashTagProps) => {
             : { border: "1px solid #d9d9d9" }
         }
       >
-        <div className={listTag?.length ? "tags-list" : ""}>
-          {listTag.map((tag, index) => {
+        <div className={tagList?.length ? "tags-list" : ""}>
+          {tagList.map((tag, index) => {
             const isLongTag = tag.length > 20;
             const tagElem = (
-              <Tag
-                key={tag}
-                closable
-                onClose={() => handleDelete(index)}
-              >
-                <span>
-                  {isLongTag ? `${tag.slice(0, 20)}...` : tag}
-                </span>
-              </Tag>
+              <span key={index} className="tag">
+                <span>{isLongTag ? `${tag.slice(0, 20)}...` : tag}</span>
+                <img alt="" onClick={() => handleDelete(index)} src={xCloseBtn} />
+              </span>
             );
 
             return isLongTag ? (
-              <Tooltip title={tag} key={tag}>
+              <Tooltip title={tag} key={index}>
                 {tagElem}
               </Tooltip>
             ) : (
