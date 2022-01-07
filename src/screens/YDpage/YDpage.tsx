c78e70c33c37dfcadd4c445a69f217760355cfac
nguyen phuong <nguyenphuong.2661999@gmail.com>
2022-01-07 09:53:30 +0000

    Resolve SC-1050 "Feature//refresh page"

--- src/screens/YDpage/YDpage.tsx
+++ src/screens/YDpage/YDpage.tsx
@@ -1,16 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import ContentContainer from "component/container/content.container";
 
-import {StyledYDpage} from "screens/YDpage/StyledYDpage";
+import { StyledYDpage } from "screens/YDpage/StyledYDpage";
 import YDpageConnect from "assets/img/ydpage-connect.svg";
 import FacebookIcon from "assets/icon/facebook.svg";
 import InstagramIcon from "assets/icon/instagram.svg";
 import ZaloIcon from "assets/icon/zalo.svg";
 
-import {AppConfig} from "config/app.config";
-import {useQuery} from "utils/useQuery";
-import {getYdpageSource, setYdpageSource} from "utils/LocalStorageUtils";
+import { AppConfig } from "config/app.config";
+import { useQuery } from "utils/useQuery";
+import { getYdpageSource, setYdpageSource } from "utils/LocalStorageUtils";
 
 const YDPAGE_URL = AppConfig.ydPageUrl;
 const allSource = {
@@ -22,10 +22,36 @@ const YDpage: React.FC = () => {
     setYdpageSource(allSource.FACEBOOK);
   };
 
+  function setYdpagePath(path: any) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('path', path);
+    window.history.pushState({}, '', url);
+  }
+
+  function getYdPageUrl() {
+    const pathParam = new URLSearchParams(window.location.search).get('path');
+    const path = (pathParam) ? pathParam : '/';
+    return new URL(YDPAGE_URL || '').origin + '/#' + path;
+  }
+
+  window.addEventListener("message", (event) => {
+    const { data } = event;
+    const { cmd, route } = data;
+
+    switch (cmd) {
+      case 'save_route_path':
+        setYdpagePath(route.path);
+        break;
+      default:
+        break;
+    }
+  }, false);
+
   const queryString = useQuery();
   const fbCode = queryString.get("code");
 
   const [source, setSource] = useState<String | null>(getYdpageSource());
+
   return (
     <StyledYDpage>
       {!source && (
@@ -46,16 +72,16 @@ const YDpage: React.FC = () => {
                     <img
                       className="image"
                       src={FacebookIcon}
-                      style={{marginRight: "15px", height: "40px"}}
+                      style={{ marginRight: "15px", height: "40px" }}
                       alt=""
                     />
                     <span className="text-button">Facebook</span>
                   </div>
-                  <div className="social-button" style={{display: "none"}}>
+                  <div className="social-button" style={{ display: "none" }}>
                     <img
                       className="image"
                       src={InstagramIcon}
-                      style={{marginRight: "15px", height: "40px"}}
+                      style={{ marginRight: "15px", height: "40px" }}
                       alt=""
                     />
                     <span className="text-button">Instagram</span>
@@ -63,11 +89,11 @@ const YDpage: React.FC = () => {
                 </div>
 
                 <div className="row-button">
-                  <div className="social-button" style={{display: "none"}}>
+                  <div className="social-button" style={{ display: "none" }}>
                     <img
                       className="image"
                       src={ZaloIcon}
-                      style={{marginRight: "15px", height: "40px"}}
+                      style={{ marginRight: "15px", height: "40px" }}
                       alt=""
                     />
                     <span className="text-button">Zalo</span>
@@ -82,8 +108,8 @@ const YDpage: React.FC = () => {
         <iframe
           className="ydpage-iframe"
           title="ydpage"
-          src={YDPAGE_URL}
-          style={{width: "100%", height: "100%"}}
+          src={getYdPageUrl()}
+          style={{ width: "100%", height: "100%" }}
         ></iframe>
       )}
       {source === allSource.FACEBOOK && fbCode && (
@@ -91,7 +117,7 @@ const YDpage: React.FC = () => {
           className="ydpage-iframe"
           title="ydpage"
           src={`${YDPAGE_URL}auth/facebook/callback?code=${fbCode}`}
-          style={{width: "100%", height: "100%"}}
+          style={{ width: "100%", height: "100%" }}
         ></iframe>
       )}
     </StyledYDpage>
