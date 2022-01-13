0de6af58c7b8e6a5bdc1e88c82c07e6f4830ba4a
Nhật Nguyễn <nhatnm28@gmail.com>
2022-01-13 02:00:01 +0000

    Resolve CO-1774

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,47 +1,58 @@
 import AuthRoute from "component/auth.route";
 import UrlConfig from "config/url.config";
 import { RouteMenu } from "model/other";
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Switch, useLocation } from "react-router-dom";
 import Login from "screens/login";
 import menu from "./menu";
 import extra from "./menu/extra";
 import routesNotShowInMenu from "./list-routes-not-show-in-menu";
+import ReactGA from "react-ga";
+const NotFoundScreen = React.lazy(() => import("screens/notfound.screen"));
 
-const  NotFoundScreen = React.lazy(() => import ('screens/notfound.screen'));
+/**
+ * @description: tracking data for google analytics
+ */
+const GA_TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+if (GA_TRACKING_ID) {
+  ReactGA.initialize(GA_TRACKING_ID);
+}
+
 /**
  * route dạng /:id cần phải để phía sau tránh bị ghi đè
- * VD: có products/:id và products/variants 
- * nếu để products/:id trước thì products/variants sẽ không được match 
+ * VD: có products/:id và products/variants
+ * nếu để products/:id trước thì products/variants sẽ không được match
  */
-const childRoute : Array<RouteMenu> = []; 
+const childRoute: Array<RouteMenu> = [];
 
 const listMenu = () => {
   let list: Array<RouteMenu> = [];
-  menu.forEach((item) => list = [...list, ...getAllRoute(item)]);
+  menu.forEach((item) => (list = [...list, ...getAllRoute(item)]));
   return list;
-}
+};
 
 const listExtraMenu = () => {
   let list: Array<RouteMenu> = [];
-  extra.forEach((item) => list = [...list, ...getAllRoute(item)]);
+  extra.forEach((item) => (list = [...list, ...getAllRoute(item)]));
   return list;
-}
+};
 
 const listRoutesNotShowInMenu = () => {
   let list: Array<RouteMenu> = [];
-  routesNotShowInMenu.forEach((item) => list = [...list, ...getAllRoute(item)]);
+  routesNotShowInMenu.forEach(
+    (item) => (list = [...list, ...getAllRoute(item)])
+  );
 
   return list;
-}
+};
 
 const getAllRoute = (route: RouteMenu) => {
   let temps: Array<RouteMenu> = [];
-  if(route.subMenu.length > 0) {
+  if (route.subMenu.length > 0) {
     route.subMenu.forEach((subItem: RouteMenu) => {
-      let menu = getAllRoute(subItem)
-      temps = [...temps, ...menu]
-    })
+      let menu = getAllRoute(subItem);
+      temps = [...temps, ...menu];
+    });
   }
   if (route.isShow) {
     if (route.path.includes(":")) {
@@ -49,37 +60,68 @@ const getAllRoute = (route: RouteMenu) => {
     } else {
       temps.push(route);
     }
-  }  
+  }
   return temps;
-}
+};
 
 const MainRoute = () => {
+  
+  /**
+   * @description: tracking data for google analytics
+   */
+  const location = useLocation();
+  useEffect(() => {
+    if (GA_TRACKING_ID && location) {
+      ReactGA.pageview(location.pathname + location.search);
+    }
+  }, [location]);
+
   return (
     <Switch>
-      {
-        listRoutesNotShowInMenu().map((item: RouteMenu) => (
-          <AuthRoute key={item.key} component={item.component} exact={item.exact} path={item.path} title={item.title} permissions={item.permissions}/>
-        ))
-      }
-      {
-        listMenu().map((item: RouteMenu) => (
-          <AuthRoute key={item.key} component={item.component} exact={item.exact} path={item.path} title={item.title} permissions={item.permissions}/>
-        ))
-      }
-      {
-        listExtraMenu().map((item: RouteMenu) => (
-          <AuthRoute key={item.key} component={item.component} exact={item.exact} path={item.path} title={item.title}  permissions={item.permissions}/>
-        ))
-      }
-      {
-        childRoute.map((item: RouteMenu) => (
-          <AuthRoute key={item.key} component={item.component} exact={item.exact} path={item.path} title={item.title}  permissions={item.permissions}/>
-        ))
-      }
+      {listRoutesNotShowInMenu().map((item: RouteMenu) => (
+        <AuthRoute
+          key={item.key}
+          component={item.component}
+          exact={item.exact}
+          path={item.path}
+          title={item.title}
+          permissions={item.permissions}
+        />
+      ))}
+      {listMenu().map((item: RouteMenu) => (
+        <AuthRoute
+          key={item.key}
+          component={item.component}
+          exact={item.exact}
+          path={item.path}
+          title={item.title}
+          permissions={item.permissions}
+        />
+      ))}
+      {listExtraMenu().map((item: RouteMenu) => (
+        <AuthRoute
+          key={item.key}
+          component={item.component}
+          exact={item.exact}
+          path={item.path}
+          title={item.title}
+          permissions={item.permissions}
+        />
+      ))}
+      {childRoute.map((item: RouteMenu) => (
+        <AuthRoute
+          key={item.key}
+          component={item.component}
+          exact={item.exact}
+          path={item.path}
+          title={item.title}
+          permissions={item.permissions}
+        />
+      ))}
       <Route path={UrlConfig.LOGIN} exact={true} component={Login} />
       <Route component={NotFoundScreen} />
     </Switch>
-  )
-}
+  );
+};
 
 export default MainRoute;
