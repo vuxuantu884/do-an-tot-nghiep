5ef42d1b726810b34275bce2c06ec8d6605a7508
FE - Minh Nhat <nhatnm28@gmail.com>
2022-01-19 18:07:34 +0700

    done CO-1995

--- src/component/filter/account.filter.tsx
+++ src/component/filter/account.filter.tsx
@@ -93,14 +93,23 @@ const AccountFilter: React.FC<AccountFilterProps> = (props: AccountFilterProps) 
 
   return (
     <div>
-      <Form form={formRef} onFinish={onFinish} initialValues={{ ...params, from_date: parseStringToDate(params?.from_date?.toString()), to_date: parseStringToDate(params?.to_date?.toString()) }} layout="inline">
+      <Form
+        form={formRef}
+        onFinish={onFinish}
+        initialValues={{
+          ...params,
+          from_date: parseStringToDate(params?.from_date?.toString()),
+          to_date: parseStringToDate(params?.to_date?.toString()),
+        }}
+        layout="inline">
         <FilterWrapper>
           <Form.Item name="info" className="search" style={{ minWidth: 200 }}>
-            <Input prefix={<img src={search} alt="" />} placeholder="Tên/Mã nhân viên" />
+            <Input
+              prefix={<img src={search} alt="" />}
+              placeholder="Tên/Mã nhân viên"
+            />
           </Form.Item>
-          <Form.Item name="store_ids" style={{ minWidth: 220 }}>
-            <TreeStore name="store_ids" listStore={listStore} form={formRef} />
-          </Form.Item>
+
           <Form.Item name="department_ids">
             <Select
               showSearch
@@ -110,25 +119,37 @@ const AccountFilter: React.FC<AccountFilterProps> = (props: AccountFilterProps) 
               optionFilterProp="title"
               style={{
                 width: 250,
-              }}
-            >
+              }}>
               {listDepartment &&
                 listDepartment.map((single: any) => {
                   return (
-                    <Select.Option value={single.id} key={single.id} title={single.name}>
+                    <Select.Option
+                      value={single.id}
+                      key={single.id}
+                      title={single.name}>
                       <span
                         className="hideInSelect"
-                        style={{ paddingLeft: +18 * single.level }}
-                      ></span>
+                        style={{ paddingLeft: +18 * single.level }}></span>
                       {single?.parent?.name && (
                         <span className="hideInDropdown">
                           {single?.parent?.name} -{" "}
                         </span>
                       )}
-                      <span className={`${single.level === 0 && "itemParent"}`}>{single.name}</span>
+                      <span className={`${single.level === 0 && "itemParent"}`}>
+                        {single.name}
+                      </span>
                     </Select.Option>
                   );
                 })}
+            </Select>
+          </Form.Item>
+          <Form.Item name="status" style={{ minWidth: 220 }}>
+            <Select showArrow placeholder="Trạng thái" allowClear>
+              {listStatus?.map((item) => (
+                <Select.Option key={item.value} value={item.value}>
+                  {item.name}
+                </Select.Option>
+              ))}
             </Select>
           </Form.Item>
           <Form.Item>
@@ -148,58 +169,84 @@ const AccountFilter: React.FC<AccountFilterProps> = (props: AccountFilterProps) 
           onFilter={onFilterClick}
           onCancel={onCancelFilter}
           visible={visible}
-          width={396}
-        >
+          width={396}>
           <FilterAccountAdvancedStyles>
-            {/* <Form onFinish={onFinish} form={formRef} initialValues={{...params,from_date: moment(params.from_date), to_date:moment(params.to_date) }}  layout="vertical"> */}
             <div className="ant-form ant-form-vertical">
-            <Row gutter={50}>
-              <Col span={24}>
-                <Row className="filter-date">
-                  <Form.Item name="from_date" label="Thời gian tạo từ" className="filter-date__from">
-                    <DatePicker placeholder="20/01/2021" format="DD/MM/YYYY" />
+              <Row gutter={50}>
+                <Col span={24}>
+                  <Row className="filter-date">
+                    <Form.Item
+                      name="from_date"
+                      label="Thời gian tạo từ"
+                      className="filter-date__from">
+                      <DatePicker
+                        placeholder="20/01/2021"
+                        format="DD/MM/YYYY"
+                      />
+                    </Form.Item>
+                    <CgArrowRight />
+                    <Form.Item
+                      name="to_date"
+                      label="Đến"
+                      className="filter-date__from">
+                      <DatePicker
+                        placeholder="25/01/2021"
+                        format="DD/MM/YYYY"
+                      />
+                    </Form.Item>
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <Form.Item name="position_ids" label="Vị trí">
+                    <Select showArrow placeholder="Vị trí" allowClear>
+                      {listPosition?.map((item) => (
+                        <Select.Option key={item.id} value={item.id.toString()}>
+                          {item.name}
+                        </Select.Option>
+                      ))}
+                    </Select>
                   </Form.Item>
-                  <CgArrowRight />
-                  <Form.Item name="to_date" label="Đến" className="filter-date__from">
-                    <DatePicker placeholder="25/01/2021" format="DD/MM/YYYY" />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <Form.Item name="mobile" label="Số điện thoại">
+                    <Input placeholder="Số điện thoại" allowClear />
                   </Form.Item>
-                </Row>
-              </Col>
-            </Row>
-            <Row gutter={50}>
-              <Col span={24}>
-                <Form.Item name="position_ids" label="Vị trí">
-                  <Select showArrow placeholder="Vị trí" allowClear>
-                    {listPosition?.map((item) => (
-                      <Select.Option key={item.id} value={item.id.toString()}>
-                        {item.name}
-                      </Select.Option>
-                    ))}
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={50}>
-              <Col span={24}>
-                <Form.Item name="mobile" label="Số điện thoại">
-                  <Input placeholder="Số điện thoại" allowClear />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={50}>
-              <Col span={24}>
-                <Form.Item name="status" label="Trạng thái">
-                  <Select showArrow placeholder="Trạng thái" allowClear>
-                    {listStatus?.map((item) => (
-                      <Select.Option key={item.value} value={item.value}>
-                        {item.name}
-                      </Select.Option>
-                    ))}
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row></div>
-            {/* </Form> */}
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <Form.Item name="store_ids" label="Cửa hàng">
+                    <TreeStore
+                      name="store_ids"
+                      listStore={listStore}
+                      form={formRef}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              {/* <Row>
+                <Col span={24}>
+                  <Form.Item label="Phân quyền chi tiết">
+                    <Checkbox.Group className="group-checkbox-btn">
+                      <Checkbox
+                        value={true}
+                        className="group-checkbox-btn__checkbox">
+                        <div className="ant-btn">Có</div>
+                      </Checkbox>
+                      <Checkbox
+                        value={false}
+                        className="group-checkbox-btn__checkbox">
+                        <div className="ant-btn">Không</div>
+                      </Checkbox>
+                    </Checkbox.Group>
+                  </Form.Item>
+                </Col>
+              </Row> */}
+            </div>
           </FilterAccountAdvancedStyles>
         </BaseFilter>
       </Form>
