54904fe66ed5ee016303619fc9ba4a9d246354a8
doanhv <doan.hoa@tripath.vn>
2022-02-18 17:59:05 +0700

    refactor select paging v2, custom select search paging

--- src/component/custom/SelectPaging/SelectPagingV2.tsx
+++ src/component/custom/SelectPaging/SelectPagingV2.tsx
@@ -1,9 +1,9 @@
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Button, Row, Select as ANTSelect, SelectProps, Space } from "antd";
 import { AppConfig } from "config/app.config";
-import _ from "lodash";
+import debounce from "lodash/debounce";
 import { BaseMetadata } from "model/base/base-metadata.response";
-import React, { CSSProperties, ReactNode, useState } from "react";
+import React, {CSSProperties, ReactNode, useState} from "react";
 
 interface Props extends SelectProps<any> {
   style?: CSSProperties;
@@ -25,39 +25,52 @@ const SelectPagingV2 = (props: Props) => {
   const totalPage = metadata ? Math.ceil((metadata.total || 1) / (metadata.limit || 1)) : 1;
   const [key, setKey] = useState("");
 
+  const onChange = (type: 'next' | 'prev') => {
+    let newPage = type === 'next' ? metadata.page + 1 :  metadata.page - 1
+    if ( type === 'prev' && newPage >= 1 ||  type === 'next' && newPage <= totalPage) {
+      onPageChange && onPageChange(key, newPage);
+    }
+  }
+
+  const onSearchValue = (value: string) => {
+    setKey(value);
+    onSearch && onSearch(value);
+  }
+
+  const buttons = [
+    {
+      key: 'prev',
+      onClick: () => onChange('prev'),
+      disabled: metadata.page === 1,
+      arrowComponent: <LeftOutlined />
+    },
+    {
+      key: 'next',
+      onClick: () => onChange('next'),
+      disabled: metadata.page >= totalPage,
+      arrowComponent: <RightOutlined />
+    }
+  ]
+
   return (
     <ANTSelect
-      onSearch={_.debounce((value) => {
-        setKey(value);
-        onSearch?.(value);
-      }, AppConfig.TYPING_TIME_REQUEST)}
+      onSearch={debounce(onSearchValue, AppConfig.TYPING_TIME_REQUEST)}
       dropdownRender={(menu) => (
         <div>
           {menu}
           <Row justify="center" style={{ marginTop: 10 }}>
             <Space>
-              <Button
-                disabled={metadata.page === 1}
-                style={{ width: 30, height: 30, padding: 0, lineHeight: "20px" }}
-                onClick={() => {
-                  let newPage = metadata.page - 1;
-                  if (newPage >= 1) {
-                    onPageChange && onPageChange(key, newPage);
-                  }
-                }}>
-                <LeftOutlined />
-              </Button>
-              <Button
-                disabled={metadata.page >= totalPage}
-                onClick={() => {
-                  let newPage = metadata.page + 1;
-                  if (newPage <= totalPage) {
-                    onPageChange && onPageChange(key, newPage);
-                  }
-                }}
-                style={{ width: 30, height: 30, padding: 0, lineHeight: "20px" }}>
-                <RightOutlined />
-              </Button>
+              {
+                buttons.map((btn) => (
+                  <Button
+                    key={btn.key}
+                    disabled={btn.disabled}
+                    onClick={btn.onClick}
+                    style={{ width: 30, height: 30, padding: 0, lineHeight: "20px" }}>
+                    {btn.arrowComponent}
+                  </Button>
+                ))
+              }
             </Space>
           </Row>
         </div>
