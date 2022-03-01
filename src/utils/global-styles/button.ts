2ed60cb285fcd56b5547d3ea31a28c6db315f1c9
hoavandoan <hoavandoan@gmail.com>
2022-03-01 06:15:39 +0000

    [CO-2371] fix layout product, css btn

--- src/utils/global-styles/button.ts
+++ src/utils/global-styles/button.ts
@@ -22,9 +22,6 @@ export const globalCssCustomButton = css`
     color: inherit;
     border-color: ${borderColor};
     background-color: #ffffff;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
     &-icon-only {
       width: 42px;
       padding: 6px;
