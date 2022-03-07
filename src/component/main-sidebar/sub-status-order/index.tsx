b70b606abda3c7f99abf622739eba86948a6751b
manhnguyengg@gmail.com <manhnguyengg@gmail.com>
2022-03-07 11:24:16 +0700

    [OR-1836] trạng thái khi chuyển hvc update

--- src/component/main-sidebar/sub-status-order/index.tsx
+++ src/component/main-sidebar/sub-status-order/index.tsx
@@ -44,7 +44,6 @@ function SubStatusOrder(props: PropType): React.ReactElement {
 	const [listOrderSubStatus, setListOrderSubStatus] = useState<OrderSubStatusResponse[]>(
 		[]
 	);
-	const [orderReturnSubStatus, setOrderReturnSubStatus] = useState<OrderProcessingStatusModel|null>(null);
 	const [valueSubStatusCode, setValueSubStatusCode] = useState<string | undefined>(undefined);
 
 	const sortedFulfillments = useMemo(() => {
@@ -55,8 +54,19 @@ function SubStatusOrder(props: PropType): React.ReactElement {
 
 		}
 	}, [OrderDetailAllFulfillment?.fulfillments])
+	console.log('sortedFulfillments', sortedFulfillments)
 
 	const handleOrderSubStatus = useCallback((sub_status_code: string) => {
+		const removeReturnInListIfOrderFinalize = (arr: OrderSubStatusResponse[]) => {
+			console.log('sub_status_code', sub_status_code)
+			console.log('status', status)
+			if(status === OrderStatus.FINALIZED && sub_status_code !== ORDER_RETURN_SUB_STATUS.returning.code && sub_status_code !== ORDER_RETURN_SUB_STATUS.returned.code) {
+				console.log('333333333')
+				return arr.filter(status => status.code !== ORDER_RETURN_SUB_STATUS.returning.code && status.code !== ORDER_RETURN_SUB_STATUS.returned.code)
+			};
+			return arr
+		}
+
 		const STATUS_ORDER_PARTNER = [
 			// tạo nháp
 			{
@@ -144,13 +154,13 @@ function SubStatusOrder(props: PropType): React.ReactElement {
 			},
 			// đang hoàn
 			{
-				orderStatus: OrderStatus.FINISHED,
+				orderStatus: OrderStatus.FINALIZED,
 				now: "returning",
 				list: ["returning", "returned"]
 			},
 			// đã hoàn
 			{
-				orderStatus: OrderStatus.FINISHED,
+				orderStatus: OrderStatus.FINALIZED,
 				now: "returned",
 				list: ["returned"]
 			},
@@ -174,12 +184,30 @@ function SubStatusOrder(props: PropType): React.ReactElement {
 			{
 				orderStatus: OrderStatus.FINALIZED,
 				now: "awaiting_coordinator_confirmation",
-				list: ["awaiting_coordinator_confirmation", "coordinator_confirmed", "merchandise_picking", "require_warehouse_change"]
+				list: ["awaiting_coordinator_confirmation", "awaiting_saler_confirmation","coordinator_confirmed", "4h_delivery"]
 			},
 			{
 				orderStatus: OrderStatus.FINALIZED,
 				now: "merchandise_picking",
 				list: ["merchandise_picking", "require_warehouse_change"]
+			},
+			// chờ sale xác nhận lại	
+			{
+				orderStatus: OrderStatus.FINALIZED,
+				now: "awaiting_saler_confirmation",
+				list: ["awaiting_saler_confirmation", "awaiting_coordinator_confirmation"]
+			},
+			// đã xác nhận
+			{
+				orderStatus: OrderStatus.FINALIZED,
+				now: "coordinator_confirmed",
+				list: ["coordinator_confirmed", "merchandise_picking","require_warehouse_change"]
+			},
+			//đổi kho hàng
+			{
+				orderStatus: OrderStatus.FINALIZED,
+				now: "require_warehouse_change",
+				list: ["require_warehouse_change", "coordinator_confirmed","awaiting_saler_confirmation"]
 			},
 			{
 				orderStatus: OrderStatus.FINALIZED,
@@ -218,51 +246,48 @@ function SubStatusOrder(props: PropType): React.ReactElement {
 				now: "third_call_attempt",
 				list: ["third_call_attempt"]
 			},
-			{
-				orderStatus: OrderStatus.FINALIZED,
-				now: "awaiting_coordinator_confirmation",
-				list: ["awaiting_coordinator_confirmation", "awaiting_saler_confirmation", "coordinator_confirmed", "require_warehouse_change", "4h_delivery"]
-			},
 		]
-		const filterStatus = (arr: any[]) => {
-			let result: OrderSubStatusResponse[] = [...initOrderSubStatus];
+		const filterStatus = (arr: any[], arrOrderSubStatus: OrderSubStatusResponse[] ) => {
+			let result: OrderSubStatusResponse[] = [...arrOrderSubStatus];
 			const foundStatus = arr.find((single) => {
 				return single.now === sub_status_code && single.orderStatus === status
-
 			})
 			if (foundStatus) {
-				let arr: OrderSubStatusResponse[] = [];
+				let arrResult: OrderSubStatusResponse[] = [];
 				foundStatus.list.forEach((single: any) => {
-					let mapStatuses = initOrderSubStatus.find(aa => aa.code === single)
+					let mapStatuses = arrOrderSubStatus.find(status => status.code === single)
 					if (mapStatuses) {
-						arr.push(mapStatuses)
+						arrResult.push(mapStatuses)
 					}
 				})
-				result = arr;
+				result = arrResult;
+				console.log('arrResult', arrResult)
 			}
 			return result;
 		};
+		// console.log('OrderDetailAllFulfillment', OrderDetailAllFulfillment)
 		if (!OrderDetailAllFulfillment?.fulfillments) {
 			return;
 		}
 		let result = [...initOrderSubStatus];
+		result = removeReturnInListIfOrderFinalize(result)
 
 		switch (sortedFulfillments[0]?.shipment?.delivery_service_provider_type) {
 			// giao hàng hvc, tự giao hàng
 			case ShipmentMethod.EXTERNAL_SERVICE:
 			case ShipmentMethod.EMPLOYEE:
-				result = filterStatus(STATUS_ORDER_PARTNER);
+				result = filterStatus(STATUS_ORDER_PARTNER, result);
 				break;
 			// nhận tại cửa hàng
 			case ShipmentMethod.PICK_AT_STORE:
-				result = filterStatus(STATUS_ORDER_AT_STORE);
+				result = filterStatus(STATUS_ORDER_AT_STORE, result);
 				break
 			default:
-				result = filterStatus(STATUS_ORDER_OTHER);
+				result = filterStatus(STATUS_ORDER_OTHER, result);
 				break;
 		}
 		setListOrderSubStatus(result)
-	}, [OrderDetailAllFulfillment?.fulfillments, initOrderSubStatus, sortedFulfillments, status]);
+	}, [ORDER_RETURN_SUB_STATUS.returned.code, ORDER_RETURN_SUB_STATUS.returning.code, OrderDetailAllFulfillment?.fulfillments, initOrderSubStatus, sortedFulfillments, status]);
 
 	const handleChange = (sub_status_code: string) => {
 
@@ -307,44 +332,6 @@ function SubStatusOrder(props: PropType): React.ReactElement {
 			);
 		}
 	}, [dispatch, fulfillments, status]);
-
-	useEffect(() => {
-		const addReturnToList = async () => {
-			let found = undefined;
-			const isOrderReturn = (sortedFulfillments: FulFillmentResponse[] | null) => {
-				if (!sortedFulfillments || sortedFulfillments?.length === 0) {
-					return false
-				}
-				if (sortedFulfillments[0].status === FulFillmentStatus.CANCELLED || sortedFulfillments[0].status === FulFillmentStatus.RETURNED || sortedFulfillments[0].status === FulFillmentStatus.RETURNING) {
-					return true
-				}
-				return false
-			};
-			if (isOrderReturn(sortedFulfillments)) {
-				if(OrderDetailAllFulfillment?.sub_status_id) {
-					await getOrderProcessingStatusDetailService(OrderDetailAllFulfillment.sub_status_id).then((response: BaseResponse<OrderProcessingStatusModel>) => {
-						if(isFetchApiSuccessful(response)) {
-							found = response.data;
-						} else {
-							handleFetchApiError(response, "Chi tiết trạng thái xử lý đơn hàng", dispatch)
-						}
-					})
-
-				}
-				if (found) {
-					setOrderReturnSubStatus(found);
-				}
-			};
-		}
-		addReturnToList();
-	}, [OrderDetailAllFulfillment?.sub_status_id, dispatch, sortedFulfillments])
-
-	useEffect(() => {
-	 if(orderReturnSubStatus) {
-		let result = [...initOrderSubStatus, ...[orderReturnSubStatus]]
-		setListOrderSubStatus(result)
-	 }
-	}, [initOrderSubStatus, orderReturnSubStatus])
 
 	useEffect(() => {
 		if (valueSubStatusCode) {
