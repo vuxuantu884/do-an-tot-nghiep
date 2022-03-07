aa3866337a6782b22fbb438cd47333b13c16d902
hoavandoan <hoavandoan@gmail.com>
2022-03-07 21:50:20 +0700

    [CO-2469] resolve conflict local

---
+++ src/utils/supplier.ts
@@ -0,0 +1,29 @@
+import { RegUtil } from "./RegUtils";
+import { SupplierResponse } from "../model/core/supplier.model";
+
+type ValidationPhoneType = {
+  value: any;
+  callback: any;
+  phoneCurrent?: string | null;
+  phoneList?: SupplierResponse[];
+};
+export const validatePhoneSupplier = ({
+  value,
+  callback,
+  phoneCurrent,
+  phoneList,
+}: ValidationPhoneType) => {
+  if (value) {
+    if (!RegUtil.PHONE.test(value)) {
+      callback(`Số điện thoại không đúng định dạng`);
+    } else {
+      phoneList?.forEach((supplier) => {
+        if (phoneCurrent === value) callback();
+        if (supplier?.phone === value) callback(`Số điện thoại đã tồn tại`);
+      });
+      callback();
+    }
+  } else {
+    callback();
+  }
+};
