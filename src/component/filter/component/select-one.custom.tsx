efa84a0b4951e1c830c5bacfe038591077357333
Được Nguyễn Cố <ncduoc2502@gmail.com>
2022-03-10 11:47:07 +0700

    [CO-2496] thay doi trang thai bo loc nang cao san pham

--- src/component/filter/component/select-one.custom.tsx
+++ src/component/filter/component/select-one.custom.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 type CustomSelectOneProps = {
   value?: string
   data?: any,
-  onChange?: (value: string) => void;
+  onChange?: (value: any) => void;
   span?: number
 };
 
@@ -16,7 +16,7 @@ const CustomSelectOne: React.FC<CustomSelectOneProps> = (props: CustomSelectOneP
           <Button
              className={classNames("button-select", props.value === key ? "active" : "")}
              onClick={() => {
-               props.onChange && props.onChange(key)
+               props.onChange && props.onChange(key === props.value ? null: key)
              }}
             type="default"
             style={{ width: "100%", textAlign: "center", padding: 0 }}
