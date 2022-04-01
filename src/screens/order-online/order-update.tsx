29c403a3a3eab6f78c54a914c61f88fe7bc88cd1
manhnguyengg@gmail.com <manhnguyengg@gmail.com>
2022-04-01 13:51:56 +0700

    [OR-2708] hotfix fix khi update

--- src/screens/order-online/order-update.tsx
+++ src/screens/order-online/order-update.tsx
@@ -1029,7 +1029,7 @@ ShippingServiceConfigDetailResponseModel[]
 
 					setItems(responseItems);
 					setOrderAmount(
-						response.total - (response.shipping_fee_informed_to_customer || 0 ) - (OrderDetail ? getTotalOrderDiscount(OrderDetail.discounts) : 0)
+						response.total - (response.shipping_fee_informed_to_customer || 0 )
 					);
 					form.setFieldsValue({
 						...initialForm,
