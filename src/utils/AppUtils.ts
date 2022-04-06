f88d298f4b7c063165444171112d8e4b41b297b6
manhnguyengg@gmail.com <manhnguyengg@gmail.com>
2022-04-06 14:05:14 +0700

    [OR-2751][Hotfix]fix lỗi địa chỉ hà nội

--- src/utils/AppUtils.ts
+++ src/utils/AppUtils.ts
@@ -1846,6 +1846,7 @@ export const convertStringDistrict = (text: string) => {
   .replace("thanh pho", "")
   .replace("thi xa", "")
   .replace("tinh", "")
+  .replace("tp.", "")
   .replace("-", "")
 };
 
