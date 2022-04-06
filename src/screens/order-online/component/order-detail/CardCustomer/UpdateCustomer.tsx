4593d20d0a5dfb1801b02e0a82dd06bc8b0ab1ca
Mạnh Nguyễn Văn <manhnguyengg@gmail.com>
2022-04-06 05:11:10 +0000

    Resolve OR-2751 "Hotfix/ loi fill dia chi"

--- src/screens/order-online/component/order-detail/CardCustomer/UpdateCustomer.tsx
+++ src/screens/order-online/component/order-detail/CardCustomer/UpdateCustomer.tsx
@@ -37,7 +37,7 @@ import {
 import moment from "moment";
 import React, { createRef, useCallback, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { findWard, getCustomerShippingAddress, handleCalculateShippingFeeApplyOrderSetting, totalAmount } from "utils/AppUtils";
+import { convertStringDistrict, findWard, getCustomerShippingAddress, handleCalculateShippingFeeApplyOrderSetting, totalAmount } from "utils/AppUtils";
 import { GENDER_OPTIONS, VietNamId } from "utils/Constants";
 import { RegUtil } from "utils/RegUtils";
 import { showSuccess } from "utils/ToastUtils";
@@ -418,12 +418,20 @@ const UpdateCustomer: React.FC<UpdateCustomerProps> = (props) => {
   }, [customerForm]);
 
   const checkAddress = useCallback((type, value) => {
+    // trường hợp hà tĩnh thì phải replace trước khi convert
     const newValue = value.toLowerCase().replace("tỉnh", "").normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "")
       .replace(/đ/g, "d")
       .replace(/Đ/g, "D")
     // khi tìm xong tỉnh thì xóa ký tự đó để tìm huyện
-    const findArea = newAreas.find((area: any) => newValue.indexOf(area.city_name_normalize) > -1 && (newValue.indexOf(area.district_name_normalize) > -1 && newValue.replace(area.city_name_normalize, "").indexOf(area.district_name_normalize) > -1));
+    const findArea = newAreas.find((area: any) => {
+      const districtString = convertStringDistrict(area.name);
+      // tp thì xóa dấu cách thừa, tỉnh thì ko-chưa biết sao: 
+      // test Thị xã Phú Mỹ, bà rịa vũng tàu
+      // test khu một thị trấn lam Sơn huyện thọ Xuân tỉnh thanh hoá
+      const cityString = convertStringDistrict(area.city_name).replace(/\s\s+/g, ' ');
+      return newValue.indexOf(cityString) > -1 && (newValue.indexOf(districtString) > -1 && newValue.replace(cityString, "").indexOf(districtString) > -1)
+    });
     if (findArea) {
       switch (type) {
         case "full_address":
