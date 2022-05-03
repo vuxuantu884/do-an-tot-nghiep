b41278471d71ec9e9a4b6c1b234d9966037ce08c
manhnguyengg@gmail.com <manhnguyengg@gmail.com>
2022-05-03 08:46:08 +0700

    [OR-2952] hotfix: bộ lọc danh sách logic

--- src/hook/table/useSetTableColumns.ts
+++ src/hook/table/useSetTableColumns.ts
@@ -1,6 +1,8 @@
 import { FilterConfig } from "model/other";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { ICustomTableColumType } from "screens/ecommerce/table/CustomTable";
+import { COLUMN_CONFIG_TYPE } from "utils/Constants";
+import { ORDER_TYPES } from "utils/Order.constants";
 
 function useSetTableColumns(
   columnType: string,
@@ -9,9 +11,18 @@ function useSetTableColumns(
   setColumns: (columns: ICustomTableColumType<any>[]) => void
 ) {
 
+  
+  const orderVariable = useMemo(() => {
+    const orderArr = [COLUMN_CONFIG_TYPE.orderOffline, COLUMN_CONFIG_TYPE.orderOnline]
+    if(orderArr.includes(columnType)) {
+      return initColumns
+    }
+    return null
+  }, [columnType, initColumns])
+
   useEffect(() => {
     const userConfigTableColumn = tableColumnConfigs.find((e) => e.type === columnType);
-    if (userConfigTableColumn && initColumns.length > 0) {
+    if (userConfigTableColumn) {
       const config = JSON.parse(userConfigTableColumn?.json_content) as Array<
         ICustomTableColumType<any>
       >;
@@ -23,9 +34,9 @@ function useSetTableColumns(
       })
       setColumns(columnResult);
     }
-  // bỏ initColumns
+  // bỏ initColumns, thêm orderVariable 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [columnType, setColumns, tableColumnConfigs]);
+  }, [columnType, setColumns, tableColumnConfigs, orderVariable]);
 }
 
 export default useSetTableColumns;
