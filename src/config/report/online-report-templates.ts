a263cf6245b6c918318cdbb92c71c9acf2543400
HoangManh <hoangtrongmanh997@gmail.com>
2022-05-09 17:34:00 +0700

    [DT-543] fix open order detail by order code

--- src/config/report/online-report-templates.ts
+++ src/config/report/online-report-templates.ts
@@ -10,13 +10,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo nhân viên bán hàng",
-    query: `SHOW orders, return_count, ordered_item_quantity, returned_item_quantity, discounts, point_payments, total_sales, average_order_value 
-        BY assignee_code,assignee_name 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')    
+    query: `SHOW pre_orders, return_count, pre_total_sales BY assignee_code,assignee_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy') 
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
-    chart_query: `SHOW pre_total_sales BY assignee_code,assignee_name FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales BY assignee_code,assignee_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy') 
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
     cube: AnalyticCube.Sales,
@@ -29,13 +26,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo nhân viên marketing",
-    query: `SHOW orders, return_count, ordered_item_quantity, returned_item_quantity, discounts, point_payments, total_sales, average_order_value 
-        BY marketer_code,marketer_name 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online')  AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_orders, return_count, pre_total_sales BY marketer_code,marketer_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
-    chart_query: `SHOW pre_total_sales, pre_orders BY marketer_code,marketer_name FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales, pre_orders BY marketer_code,marketer_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
     cube: AnalyticCube.Sales,
@@ -48,13 +42,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo nhân viên vận đơn",
-    query: `SHOW orders, return_count, total_sales 
-        BY marketer_code,marketer_name 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')     
+    query: `SHOW pre_total_sales BY coordinator_name,coordinator_code,order_sub_status FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy') 
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
-    chart_query: `SHOW pre_total_sales BY coordinator_name,coordinator_code,order_sub_status FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales BY coordinator_name,coordinator_code,order_sub_status FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
     cube: AnalyticCube.Sales,
@@ -67,13 +58,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo kênh bán",
-    query: `SHOW orders, return_count, total_sales 
-        BY channel_provider_name,source_name 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_orders, return_count, pre_total_sales, total_sales BY channel_provider_name,source_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY total_sales DESC `,
-    chart_query: `SHOW total_sales BY channel_provider_name,source_name FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW total_sales BY channel_provider_name,source_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY total_sales DESC `,
     cube: AnalyticCube.Sales,
@@ -86,13 +74,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo đơn tạo",
-    query: `SHOW orders, return_count, total_sales, net_quantity, average_order_value, customers 
-        OVER day 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_orders, return_count, pre_total_sales, total_sales OVER day FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC`,
-    chart_query: `SHOW total_sales OVER day FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW total_sales OVER day FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC`,
     cube: AnalyticCube.Sales,
@@ -127,13 +112,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo shop",
-    query: `SHOW orders, return_count, ordered_item_quantity, returned_item_quantity, discounts, point_payments, total_sales, average_order_value 
-        BY source_department_name 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_orders, return_count, pre_total_sales, total_sales BY source_department_lv2,source_department_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
-    chart_query: `SHOW pre_total_sales BY source_department_lv2,source_department_name FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales BY source_department_lv2,source_department_name FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
     cube: AnalyticCube.Sales,
@@ -146,13 +128,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo trạng thái đơn hàng",
-    query: `SHOW orders, return_count, total_sales 
-        BY day,order_status 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_orders, pre_total_sales BY order_sub_status FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC`,
-    chart_query: `SHOW pre_total_sales, pre_orders BY order_sub_status FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales, pre_orders BY order_sub_status FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC`,
     cube: AnalyticCube.Sales,
@@ -165,13 +144,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo địa chỉ giao hàng",
-    query: `SHOW orders, return_count, net_quantity, total_sales 
-        BY shipping_city 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_orders, return_count, net_quantity, pre_total_sales, total_sales BY shipping_city FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
-    chart_query: `SHOW pre_total_sales, pre_orders BY shipping_city FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales, pre_orders BY shipping_city FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
     cube: AnalyticCube.Sales,
@@ -217,13 +193,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo sản phẩm (mã 3)",
-    query: `SHOW ordered_item_quantity, returned_item_quantity, gross_sales, returns, discounts, point_payments, total_sales 
-        BY variant_sku3 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_ordered_item_quantity, ordered_item_quantity, returned_item_quantity, pre_total_sales, total_sales BY variant_sku3 FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
-    chart_query: `SHOW pre_total_sales, pre_ordered_item_quantity BY variant_sku3 FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales, pre_ordered_item_quantity BY variant_sku3 FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
     cube: AnalyticCube.Sales,
@@ -236,13 +209,10 @@ export const ONLINE_REPORT_TEMPLATES: AnalyticTemplateData[] = [
   {
     type: "Báo cáo đơn hàng",
     name: "theo sản phẩm (mã 7)",
-    query: `SHOW total_sales, net_quantity 
-        BY variant_sku7 
-        FROM sales 
-        WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
+    query: `SHOW pre_ordered_item_quantity, ordered_item_quantity, returned_item_quantity, pre_total_sales, total_sales BY variant_sku7 FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
-    chart_query: `SHOW pre_total_sales, pre_ordered_item_quantity BY variant_sku7 FROM sales WHERE sale_area IN ('Khối KD Online') 
+    chart_query: `SHOW pre_total_sales, pre_ordered_item_quantity BY variant_sku7 FROM sales WHERE sale_area IN ('Khối KD Online') AND cancelled IN ('Chưa hủy')  
         SINCE ${START_OF_MONTH} UNTIL ${TODAY} 
         ORDER BY pre_total_sales DESC `,
     cube: AnalyticCube.Sales,
