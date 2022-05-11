2d2303ff46d6eff2af8fa9d20eaad9957baae1cc
DESKTOP-579AA2I\DELL <nhuhung19@gmail.com>
2022-05-11 13:50:53 +0700

    [CO-3105] Update xuat file excel ds pr

--- src/model/procurement/field.ts
+++ src/model/procurement/field.ts
@@ -18,12 +18,21 @@ const ProcurementLineItemField = {
     status: 'status',
     note: 'note',
     purchase_order_reference: 'purchase_order_reference',
+    supplier: 'supplier',
+    store: 'store',
+    product_code: 'product_code',
+    sku: 'sku',
+    variant: 'variant',
+    barcode: 'barcode',
+    real_quantity: 'real_quantity',
+    created_by: 'create_by',
+    created_name: 'created_name',
+    created_date: 'created_date',
     actived_date: 'actived_date',
     actived_by: 'actived_by',
     activated_by: 'activated_by',
     stock_in_date: 'stock_in_date',
     stock_in_by: 'stock_in_by',
-    store: 'store',
     is_cancelled: 'is_cancelled'
 }
 
@@ -32,6 +41,15 @@ const ProcurementExportLineItemField = {
     [ProcurementLineItemField.purchase_order_code]: 'Mã đơn đặt hàng',
     [ProcurementLineItemField.purchase_order_reference]: 'Mã tham chiếu',
     [ProcurementLineItemField.status]: 'Trạng thái',
+    [ProcurementLineItemField.supplier]: 'Nhà cung cấp',
+    [ProcurementLineItemField.store]: 'Kho hàng',
+    [ProcurementLineItemField.product_code]: 'Mã sản phẩm cha',
+    [ProcurementLineItemField.sku]: 'Mã sản phẩm',
+    [ProcurementLineItemField.variant]: 'Sản phẩm',
+    [ProcurementLineItemField.barcode]: 'Mã vạch',
+    [ProcurementLineItemField.real_quantity]: 'Số lượng nhập',
+    [ProcurementLineItemField.created_date]: 'Ngày tạo',
+    [ProcurementLineItemField.created_name]: 'Người tạo'
 }
 
 export { ProcurementField, ProcurementExportLineItemField }
