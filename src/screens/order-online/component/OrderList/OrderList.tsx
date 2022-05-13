1a13f23b69ac5a4b23dad0271c73d20e34b5fc1d
binh.phan <phanthanhbinh296@gmail.com>
2022-05-13 10:13:21 +0700

    [orders] update table

--- src/screens/order-online/component/OrderList/OrderList.tsx
+++ src/screens/order-online/component/OrderList/OrderList.tsx
@@ -1,7 +1,7 @@
 import { ExclamationCircleOutlined, PrinterOutlined } from "@ant-design/icons";
 import { Button, Card, Modal, Row, Space } from "antd";
 import exportIcon from "assets/icon/export.svg";
-import importIcon from "assets/icon/import.svg";
+// import importIcon from "assets/icon/import.svg";
 import AuthWrapper from "component/authorization/AuthWrapper";
 import ContentContainer from "component/container/content.container";
 import OrdersFilter from "component/filter/order.filter";
@@ -765,7 +765,7 @@ function OrderList(props: PropTypes) {
         extra={
           <Row>
             <Space>
-              <AuthWrapper acceptPermissions={[ODERS_PERMISSIONS.IMPORT]} passThrough>
+              {/* <AuthWrapper acceptPermissions={[ODERS_PERMISSIONS.IMPORT]} passThrough>
                 {(isPassed: boolean) => (
                   <Button
                     type="default"
@@ -778,7 +778,7 @@ function OrderList(props: PropTypes) {
                     Nhập file
                   </Button>
                 )}
-              </AuthWrapper>
+              </AuthWrapper> */}
               <AuthWrapper acceptPermissions={[ODERS_PERMISSIONS.EXPORT]} passThrough>
                 {(isPassed: boolean) => (
                   <Button
