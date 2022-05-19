d8432186fd689fd032c4c824e8b763e5423395e4
hoanggiang <hoanggiang1851996@gmail.com>
2022-05-19 08:31:35 +0000

    [co-3107] inventory filter

--- src/screens/products/constants/index.tsx
+++ src/screens/products/constants/index.tsx
@@ -3,4 +3,27 @@ export const TYPE_EXPORT = {
     page: "page",
     selected: "selected",
     allin: "allin"
-  }
+}
+
+export const documentTypes = [
+  {
+    name: 'Nhà cung cấp',
+    value: 'purchase_order',
+  },
+  {
+    name: 'Chuyển hàng',
+    value: 'inventory_transfer',
+  },
+  {
+    name: 'Đơn bán lẻ',
+    value: 'pos',
+  },
+  {
+    name: 'Đơn online',
+    value: 'order',
+  },
+  {
+    name: 'Kiểm kho',
+    value: 'inventory_adjustment',
+  },
+];
