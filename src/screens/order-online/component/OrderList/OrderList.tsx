c92fc0e568cb77eb2c053b1afad203e7ddf6fdd8
lv LloOng <lvlong96k@gmail.com>
2022-05-29 01:36:22 +0700

    fix url lỗi

--- src/screens/order-online/component/OrderList/OrderList.tsx
+++ src/screens/order-online/component/OrderList/OrderList.tsx
@@ -300,11 +300,12 @@ function OrderList(props: PropTypes) {
   const onFilterPhoneCustomer = useCallback((phone: string) => {
     let paramCopy = { ...params, search_term: phone, page: 1  };
     setPrams(paramCopy);
+    const orderLink=orderType===ORDER_TYPES.offline?UrlConfig.OFFLINE_ORDERS:UrlConfig.ORDER;
     const queryParam = generateQuery(paramCopy);
-    const path= `${location.pathname}?${queryParam}`
+    let pathname = `${process.env.PUBLIC_URL}${orderLink}?${queryParam}`;
     //history.push(`${location.pathname}?${queryParam}`);
-    window.open(path,"_blank")
-  }, [location.pathname, params]);
+    window.open(pathname,"_blank")
+  }, [orderType, params]);
 
   const onMenuClick = useCallback(
     (index: number) => {
