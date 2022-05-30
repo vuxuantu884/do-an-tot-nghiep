06078cad0fcf00751220695e8ba4752cd0fa32dd
le long <lvlong96k@gmail.com>
2022-05-30 08:04:20 +0000

    [OR-3080] ĐƠN TRẢ | Xoá đơn trả hàng

---
+++ src/screens/order-online/order-return/components/ReturnBottomBar/return-detail-bottom.tsx
@@ -0,0 +1,23 @@
+import { Button } from 'antd';
+import React from 'react';
+import { StyledComponent } from './styles';
+
+type ReturnDetailBottomType={
+    onOk?:()=>void;
+    onCancel?:()=>void;
+    hiddenButtonRemove?:boolean;
+    hiddenButtonUpdate?:boolean;
+}
+const ReturnDetailBottom:React.FC<ReturnDetailBottomType> = (props: ReturnDetailBottomType) => {
+    const { hiddenButtonRemove, hiddenButtonUpdate, onOk}= props;
+
+    return (
+        <StyledComponent>
+            <div className="bottomBar bottomBar-detail">
+                <Button hidden={hiddenButtonRemove} danger className='btn-detail' onClick={onOk}>Xóa</Button>
+                <Button hidden={hiddenButtonUpdate} type="primary" ghost className='btn-detail' id='btn-order-return'>Sửa đơn trả (F9)</Button>
+            </div>
+        </StyledComponent>
+    );
+}
+export default ReturnDetailBottom;
