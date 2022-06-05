fdae698e0766fef5b421f6a086696dccc9904575
trungbkfet91 <trungbkfet91@gmail.com>
2022-06-05 02:42:51 +0000

    fix css in to class

--- src/screens/order-online/order-return/[id]/index.tsx
+++ src/screens/order-online/order-return/[id]/index.tsx
@@ -152,9 +152,9 @@ const ScreenReturnDetail = (props: PropTypes) => {
       title: "Xác nhận xóa",
       icon: <ExclamationCircleOutlined />,
       content: (
-        <div style={{ display: "flex", lineHeight: "5px" }}>
+        <div className="yody-modal-confirm-list-code">
           Bạn có chắc chắn xóa:
-          <div style={{ marginLeft: 10, fontWeight: 500 }}>
+          <div className="yody-modal-confirm-item-code">
             <p>{OrderDetail?.code}</p>
           </div>
         </div>
