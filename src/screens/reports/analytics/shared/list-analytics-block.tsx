fa7dc3a95db5ae1ea33f09f34d610add438ab542
nguyenminhnhat <nhatnm28@gmail.com>
2022-07-19 10:14:32 +0700

    [DT-797]-fix-dynamic-import-image

--- src/screens/reports/analytics/shared/list-analytics-block.tsx
+++ src/screens/reports/analytics/shared/list-analytics-block.tsx
@@ -1,7 +1,6 @@
 import { Card, List } from "antd"
 import { AnalyticTemplateData } from "model/report/analytics.model";
 import { Link } from "react-router-dom"
-
 type Props = {
     matchPath: string;
     data: AnalyticTemplateData[];
@@ -9,18 +8,19 @@ type Props = {
 }
 
 function ListAnalyticsBlock({ matchPath, data, title }: Props) {
+    const templates = require.context('assets/icon/analytic', true, /\.(jpg|jpeg|png|svg)$/);
+    
     return <Card title={title} className='template-report'>
         <List grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 3, xl: 4, xxl: 5 }}
-
             dataSource={data || []}
-
             renderItem={(item, index) => {
                 return (
                     <Link to={`${matchPath}/${item.id}`} key={index}>
                         <List.Item className="pointer">
                             <div className={`template-report__card `}>
                                 <div className='template-report__icon '>
-                                    <img src={require(`assets/icon/analytic/${item.iconImg}`).default} alt={item.name} /></div>
+                                    <img src={templates(`./${item.iconImg}`)} alt={item.name} />
+                                    </div>
                                 <div className='template-report__type'> {item.type} </div>
                                 <div className='template-report__name'> {item.name.toUpperCase()} </div>
                             </div>
