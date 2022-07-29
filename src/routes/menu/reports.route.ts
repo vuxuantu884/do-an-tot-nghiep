ba2c71142eabdc77a76bf5eb847e51d5e89cb0da
nguyenminhnhat <nhatnm28@gmail.com>
2022-07-29 19:13:02 +0700

    [DT-728]-hide-route-key-driver-online

--- src/routes/menu/reports.route.ts
+++ src/routes/menu/reports.route.ts
@@ -1,15 +1,8 @@
-// import { ODERS_PERMISSIONS } from "config/permissions/order.permission";
 import { ReportPermissions } from "config/permissions/report.permisstion";
 import UrlConfig from "config/url.config";
 import { RouteMenu } from "model/other";
 import React from "react";
 
-// const ReportOrdersOnline = React.lazy(() => import("screens/reports/report-orders-online"));
-// const ReportOrdersOffline = React.lazy(() => import("screens/reports/report-orders-offline"));
-// const ReportTrackingItems = React.lazy(() => import("screens/reports/report-tracking-items"));
-// const ReportTransferWarehouse = React.lazy(
-//   () => import("screens/reports/report-transfer-warehouse")
-// );
 const Analytics = React.lazy(() => import("screens/reports/analytics/index"));
 const ReadCustomizeAnalytics = React.lazy(
   () => import("screens/reports/analytics/read-customize/index"),
@@ -201,77 +194,19 @@ const reports: Array<RouteMenu> = [
           },
         ],
       },
-    ],
-  },
-  {
-    path: `${UrlConfig.KEY_DRIVER_ONLINE}`,
-    exact: true,
-    title: "Báo cáo Kết quả kinh doanh online",
-    icon: "icon-dot",
-    component: KeyDriverOnline,
-    key: "menureport6",
-    isShow: true,
-    header: null,
-    subMenu: [
       {
-        path: `${UrlConfig.KEY_DRIVER_ONLINE}/:templateCode`,
+        path: `${UrlConfig.KEY_DRIVER_ONLINE}`,
         exact: true,
         title: "Báo cáo Kết quả kinh doanh online",
         icon: "icon-dot",
         component: KeyDriverOnline,
-        key: "menureport61",
+        key: "menureport6",
         isShow: true,
         header: null,
         subMenu: [],
       },
     ],
   },
-  // {
-  //   path: `/reports/sales-online`,
-  //   exact: true,
-  //   title: "Bán hàng Online",
-  //   icon: "icon-dot",
-  //   component: ReportOrdersOnline,
-  //   key: "submenureport1",
-  //   isShow: true,
-  //   header: null,
-  //   subMenu: [],
-  // },
-  // {
-  //   path: `/reports/sales-offline`,
-  //   exact: true,
-  //   title: "Bán hàng Offline",
-  //   icon: "icon-dot",
-  //   component: ReportOrdersOffline,
-  //   key: "submenureport2",
-  //   isShow: true,
-  //   header: null,
-  //   subMenu: [],
-  // },
-  // {
-  //   path: `/reports/tracking-items`,
-  //   exact: true,
-  //   title: "Theo dõi hàng về",
-  //   icon: "icon-dot",
-  //   component: ReportTrackingItems,
-  //   key: "submenureport3",
-  //   isShow: true,
-  //   header: null,
-  //   // permissions: [ODERS_PERMISSIONS.CREATE],
-  //   subMenu: [],
-  // },
-  // {
-  //   path: `/reports/transfer-warehouse`,
-  //   exact: true,
-  //   title: "Chuyển kho",
-  //   icon: "icon-dot",
-  //   component: ReportTransferWarehouse,
-  //   key: "submenureport4",
-  //   isShow: true,
-  //   header: null,
-  //   // permissions: [ODERS_PERMISSIONS.CREATE],
-  //   subMenu: [],
-  // },
 ];
 
 export default reports;
