10508cba03000a90de2dfd54dc65e291557727b0
Nguyễn Văn Long <longkm2708@gmail.com>
2022-08-28 02:34:29 +0000

    [SC-3946] update ranking customer by progress

---
+++ src/screens/customer/ranking/ProgressRankingCustomerModal.tsx
@@ -0,0 +1,167 @@
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Progress } from "antd";
+
+import NumberFormat from "react-number-format";
+import { isNullOrUndefined } from "utils/AppUtils";
+import { StyledProgressDownloadModal } from "screens/ecommerce/common/commonStyle";
+import { StyledModalFooter } from "screens/ecommerce/common/commonStyle";
+
+type ProgressImportCustomerModalType = {
+  visible: boolean;
+  isDownloading: boolean;
+  onOk: () => void;
+  onCancel: () => void;
+  progressData: any;
+  progressPercent: number;
+};
+
+const ProgressImportCustomerModal: React.FC<ProgressImportCustomerModalType> = (
+  props: ProgressImportCustomerModalType,
+) => {
+  const { visible, isDownloading, onOk, onCancel, progressData, progressPercent } = props;
+
+  const [errorData, setErrorData] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    if (progressData?.errors_msg) {
+      const errorList = progressData?.errors_msg.slice(1).split("\n");
+      setErrorData(errorList);
+    }
+  }, [progressData?.errors_msg]);
+
+  const okProgressImportCustomerModal = () => {
+    onOk && onOk();
+  };
+
+  const cancelProgressImportCustomerModal = () => {
+    onCancel && onCancel();
+  };
+
+  return (
+    <Modal
+      width="600px"
+      centered
+      visible={visible}
+      title="Chỉnh sửa hạng khách hàng"
+      okText="Xác nhận"
+      cancelText="Hủy"
+      onCancel={cancelProgressImportCustomerModal}
+      closable={false}
+      maskClosable={false}
+      footer={
+        <StyledModalFooter>
+          {isDownloading ? (
+            <Button danger onClick={cancelProgressImportCustomerModal}>
+              Hủy
+            </Button>
+          ) : (
+            <div />
+          )}
+
+          <Button type="primary" onClick={okProgressImportCustomerModal} loading={isDownloading}>
+            Xác nhận
+          </Button>
+        </StyledModalFooter>
+      }
+    >
+      <StyledProgressDownloadModal>
+        <div className="progress-body">
+          <div className="progress-count">
+            <div>
+              <div>Tổng cộng</div>
+              <div className="total-count">
+                {isNullOrUndefined(progressData?.total) ? (
+                  "--"
+                ) : (
+                  <NumberFormat
+                    value={progressData?.total}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                  />
+                )}
+              </div>
+            </div>
+
+            <div>
+              <div>Đã xử lý</div>
+              <div style={{ fontWeight: "bold" }}>
+                {isNullOrUndefined(progressData?.processed) ? (
+                  "--"
+                ) : (
+                  <NumberFormat
+                    value={progressData?.processed}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                  />
+                )}
+              </div>
+            </div>
+
+            <div>
+              <div>Thành công</div>
+              <div className="total-updated">
+                {isNullOrUndefined(progressData?.success) ? (
+                  "--"
+                ) : (
+                  <NumberFormat
+                    value={progressData?.success}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                  />
+                )}
+              </div>
+            </div>
+
+            <div>
+              <div>Lỗi</div>
+              <div className="total-error">
+                {isNullOrUndefined(progressData?.error) ? (
+                  "--"
+                ) : (
+                  <NumberFormat
+                    value={progressData?.error}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
+          <Progress
+            status={`${progressPercent === 100 ? "normal" : "active"}`}
+            percent={progressPercent}
+            style={{ marginTop: 20 }}
+            strokeColor="#2A2A86"
+          />
+          <span style={{ color: "#ff4d4f", marginTop: 10 }}>
+            Cảnh báo: Quá trình cập nhập có thể mất thời gian.Ban vui lòng đợi chút!
+          </span>
+        </div>
+
+        {errorData.length ? (
+          <div className="error-orders">
+            <div className="title">Chi tiết lỗi:</div>
+            <div className="error_message">
+              <div style={{ backgroundColor: "#F5F5F5", padding: "20px 30px" }}>
+                <ul style={{ color: "#E24343" }}>
+                  {errorData.map((error, index) => (
+                    <li key={index} style={{ marginBottom: "5px" }}>
+                      <span style={{ fontWeight: 500 }}>{error.split(":")[0]}</span>
+                      <span>:</span>
+                      <span>{error.split(":")[1]}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div />
+        )}
+      </StyledProgressDownloadModal>
+    </Modal>
+  );
+};
+
+export default ProgressImportCustomerModal;
