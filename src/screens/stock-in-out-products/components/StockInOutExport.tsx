1a4ca17b9ef4c5404d79296fc3c6a8b4210808fd
hoanggiang <hoanggiang1851996@gmail.com>
2022-08-30 12:32:04 +0000

    [CO-4391] export stock in out

---
+++ src/screens/stock-in-out-products/components/StockInOutExport.tsx
@@ -0,0 +1,90 @@
+import { Modal, Form, Radio, Space, Row, Progress } from "antd";
+import { useCallback, Fragment } from "react";
+import { TYPE_EXPORT } from "screens/products/constants";
+import { STATUS_IMPORT_EXPORT } from "utils/Constants";
+
+type ExportModalProps = {
+  visible: boolean;
+  isLoading: boolean;
+  onCancel: () => void;
+  onOk: (record: string) => void;
+  exportProgress?: number;
+  statusExport?: number;
+};
+
+const StockInOutExport: React.FC<ExportModalProps> = (props: ExportModalProps) => {
+  const [form] = Form.useForm();
+  const { visible, onCancel, onOk, exportProgress, statusExport = 0, isLoading } = props;
+  const onCancelClick = useCallback(() => {
+    onCancel();
+  }, [onCancel]);
+  const onOkClick = useCallback(() => {
+    onOk(form.getFieldValue("record"));
+  }, [onOk, form]);
+
+  return (
+    <Modal
+      confirmLoading={isLoading}
+      onCancel={onCancelClick}
+      width={600}
+      visible={visible}
+      cancelText="Hủy"
+      onOk={onOkClick}
+      title="Xuất file nhập xuất khác"
+      okText="Xuất file"
+    >
+      <Form
+        form={form}
+        initialValues={{
+          record: TYPE_EXPORT.page,
+        }}
+        layout="vertical"
+      >
+        <Space direction="vertical" style={{ width: "100%" }}>
+          {statusExport === 0 && (
+            <Fragment>
+              <div className="title-address">Giới hạn kết quả xuất</div>
+              <Form.Item name="record">
+                <Radio.Group>
+                  <Space direction="vertical">
+                    <Radio value={TYPE_EXPORT.page}>Các phiếu trên trang này</Radio>
+                    <Radio value={TYPE_EXPORT.selected}>Các phiếu được chọn</Radio>
+                    <Radio value={TYPE_EXPORT.all}>Tất cả phiếu chuyển theo điều kiện lọc</Radio>
+                    <Radio value={TYPE_EXPORT.allin}>Tất cả phiếu nhập khác</Radio>
+                    <Radio value={TYPE_EXPORT.all_out}>Tất cả phiếu xuất khác</Radio>
+                  </Space>
+                </Radio.Group>
+              </Form.Item>
+            </Fragment>
+          )}
+          {statusExport !== 0 && statusExport === STATUS_IMPORT_EXPORT.DEFAULT && (
+            <Row style={{ justifyContent: "center" }}>
+              <p>Đang gửi yêu cầu, vui lòng đợi trong giây lát ...</p>
+            </Row>
+          )}
+          {statusExport !== 0 && statusExport !== STATUS_IMPORT_EXPORT.DEFAULT && (
+            <Row style={{ justifyContent: "center" }}>
+              {statusExport === STATUS_IMPORT_EXPORT.CREATE_JOB_SUCCESS && (
+                <p>Đang tạo file, vui lòng đợi trong giây lát</p>
+              )}
+              {statusExport === STATUS_IMPORT_EXPORT.JOB_FINISH && <p>Đã tạo file thành công</p>}
+              {statusExport === STATUS_IMPORT_EXPORT.ERROR && <p>Đã có lỗi xảy ra!!!</p>}
+              <Row style={{ justifyContent: "center", width: "100%" }}>
+                <Progress
+                  type="circle"
+                  strokeColor={{
+                    "0%": "#108ee9",
+                    "100%": "#87d068",
+                  }}
+                  percent={exportProgress}
+                />
+              </Row>
+            </Row>
+          )}
+        </Space>
+      </Form>
+    </Modal>
+  );
+};
+
+export default StockInOutExport;
