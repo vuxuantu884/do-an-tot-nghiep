4cba0c585fadc3dd2cbd614e0c07ebcd349d9ecd
Phan Bình <phanthanhbinh296@gmail.com>
2022-09-30 07:32:14 +0000

    Resolve DT-1355 and DT-1356

--- src/screens/reports/key-driver-online/index.tsx
+++ src/screens/reports/key-driver-online/index.tsx
@@ -32,6 +32,7 @@ import {
   DEFAULT_KEY_DRIVER_GROUP_LV_1,
   getAllDepartmentByAnalyticResult,
   getBreadcrumbByLevel,
+  saveActualDay,
   // getInputTargetId,
   // handleMoveFocusInput,
   saveMonthTargetKeyDriver,
@@ -519,13 +520,145 @@ function KeyDriverOnline() {
             width: 120,
             align: "right",
             dataIndex: `${departmentKey}_daily_actual`,
-            className: "non-input-cell",
+            className: "input-cell",
             render: (text: any, record: KeyDriverOnlineDataSourceType, index: number) => {
-              return (
-                <VerifyCell row={record} value={text}>
-                  {formatCurrency(text)} {record.unit === "percent" ? "%" : ""}
-                </VerifyCell>
-              );
+              if (
+                (record.key === "ON.DT.ZA.33" ||
+                  record.key === "ON.DT.MK.23" ||
+                  record.key === "ON.DT.MK.24") &&
+                departmentLv2 &&
+                !departmentLv3 &&
+                departmentKey.replaceAll(" ", "") !== departmentLv2.replaceAll(" ", "")
+              ) {
+                const targetDrillingLevel = +record[`target_drilling_level`];
+                // const inputId = getInputTargetId(index, columnIndex * 2 + 1, PREFIX_CELL_TABLE);s
+                const inputId = `${record.key}-${index}-${columnIndex * 2 + 1}-day-actual`;
+                let newValue = text ? Number(text) : 0;
+                let clickCancel = false;
+                return (
+                  <VerifyCell row={record} value={text} type="edit">
+                    <div style={{ position: "relative" }}>
+                      <NumberInput
+                        id={inputId}
+                        disabled={departmentDrillingLevel > targetDrillingLevel}
+                        value={newValue}
+                        onPressEnter={(e: any) => {
+                          const input: any = document.getElementById(inputId);
+                          input.blur();
+                        }}
+                        onFocus={(e) => {
+                          document.getElementById(`${inputId}-action`)?.removeAttribute("hidden");
+                          const input: any = document.getElementById(inputId);
+                          input.style.border = "solid 1px #ddd";
+                        }}
+                        onBlur={(e) => {
+                          setTimeout(() => {
+                            const input: any = document.getElementById(inputId);
+                            const value = input.value
+                              ? parseLocaleNumber(input.value)
+                              : parseLocaleNumber(newValue);
+                            console.log("value, newValue", value, newValue);
+
+                            if (!clickCancel && value != newValue) {
+                              newValue = value;
+                              console.log(
+                                "departmentKey",
+                                department,
+                                departmentLv2,
+                                departmentLv3,
+                              );
+                              const params: any = {
+                                department_lv1: "TỔNG CÔNG TY",
+                                department_lv2: departmentLv2,
+                                department_lv3: department,
+                                department_lv4: null,
+                                driver_key: record.key,
+                                value,
+                              };
+
+                              saveActualDay(
+                                params.department_lv1,
+                                params.department_lv2,
+                                params.department_lv3,
+                                params.department_lv4,
+                                params.driver_key,
+                                params.value,
+                                dispatch,
+                              );
+                            } else {
+                              clickCancel = false;
+                            }
+                            input.style.border = "none";
+                            document
+                              .getElementById(`${inputId}-action`)
+                              ?.setAttribute("hidden", "false");
+                          }, 100);
+                        }}
+                        onKeyDown={(e: any) => {
+                          // if (e.shiftKey) {
+                          //   handleMoveFocusInput(
+                          //     index,
+                          //     columnIndex * 2 + 1,
+                          //     PREFIX_CELL_TABLE,
+                          //     e.key,
+                          //   );
+                          // }
+                          if (e.key === "Escape") {
+                            const event = new Event("input", { bubbles: true });
+                            const input: any = document.getElementById(inputId);
+                            setNativeValue(input, newValue);
+                            input.dispatchEvent(event);
+                            input.blur();
+                          }
+                        }}
+                        suffix={record.unit === "percent" ? "%" : null}
+                        {...inputTargetDefaultProps}
+                      />
+                      <div
+                        id={`${inputId}-action`}
+                        hidden
+                        style={{
+                          display: "flex",
+                          justifyContent: "flex-end",
+                          position: "absolute",
+                          zIndex: 2,
+                          paddingTop: "2px",
+                          right: 0,
+                        }}
+                      >
+                        <Button
+                          size="small"
+                          icon={<CloseOutlined style={{ verticalAlign: "0.25em" }} />}
+                          style={{ borderColor: "#ddd", backgroundColor: "#fff" }}
+                          onClick={(e) => {
+                            clickCancel = true;
+                            const event = new Event("input", { bubbles: true });
+                            const input: any = document.getElementById(inputId);
+                            setNativeValue(input, newValue);
+                            input.dispatchEvent(event);
+                            input.blur();
+                          }}
+                        />
+                        <Button
+                          size="small"
+                          icon={<CheckOutlined style={{ verticalAlign: "0.25em" }} />}
+                          style={{ marginLeft: 5, borderColor: "#ddd", backgroundColor: "#fff" }}
+                          onClick={(e) => {
+                            const input: any = document.getElementById(inputId);
+                            input.blur();
+                          }}
+                        />
+                      </div>
+                    </div>
+                  </VerifyCell>
+                );
+              } else {
+                return (
+                  <VerifyCell row={record} value={text}>
+                    {formatCurrency(text)} {record.unit === "percent" ? "%" : ""}
+                  </VerifyCell>
+                );
+              }
             },
           },
           {
@@ -545,7 +678,7 @@ function KeyDriverOnline() {
         ],
       };
     },
-    [day, dispatch, targetDay],
+    [day, departmentLv2, departmentLv3, dispatch, targetDay],
   );
 
   const initTable = useCallback(
