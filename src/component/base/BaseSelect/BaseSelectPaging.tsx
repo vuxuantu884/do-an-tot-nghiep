9758dce163e99e8a4dce35e87f57d0ad1c86941a
tuvx <vuxuantuyody@gmail.com>
2022-12-09 17:47:22 +0700

    [Co-5673] hotfix| Kiểm tra search màu sắc trong tạo sp--> Chỉ lấy 30 bản ghi đầu của màu tìm kiếm

--- src/component/base/BaseSelect/BaseSelectPaging.tsx
+++ src/component/base/BaseSelect/BaseSelectPaging.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseSelect from "./BaseSelect";
 import { BaseSelectPagingType } from "./BaseSelect.type";
 import debounce from "lodash/debounce";
@@ -10,16 +10,19 @@ function BaseSelectPaging<T>({
   fetchData,
   valueSearch,
   onDeselect,
+  onSearch,
   ...props
 }: BaseSelectPagingType<T>) {
   const totalPage = metadata ? Math.ceil((metadata.total || 1) / (metadata.limit || 1)) : 1;
 
   const onSearchValue = (value: string) => {
+    onSearch && onSearch(value);
     fetchData && fetchData({ condition: value.trim(), page: 1 });
   };
   const onChange = (type: "next" | "prev") => {
     let newPage = type === "next" ? metadata.page + 1 : metadata.page - 1;
     if ((type === "prev" && newPage >= 1) || (type === "next" && newPage <= totalPage)) {
+      console.log("value", valueSearch);
       fetchData({
         [`${valueSearch ? "info" : "condition"}`]: valueSearch?.trim(),
         page: newPage,
@@ -30,6 +33,7 @@ function BaseSelectPaging<T>({
     <BaseSelect<T>
       optionFilterProp="children"
       showSearch
+      valueSearch={valueSearch}
       onSearch={debounce(onSearchValue, AppConfig.TYPING_TIME_REQUEST)}
       filterOption={false}
       {...props}
