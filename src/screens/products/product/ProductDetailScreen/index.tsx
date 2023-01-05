a80b1736a93423ddb730d74b4d898a164b741ba9
hungtn <nhuhung19@gmail.com>
2023-01-05 14:52:03 +0700

    Revert "Revert "Merge branch 'feature/CO-6737-search-stores-inventory-product-screen' into 'release/v1.19.0'""
    This reverts commit 386c2c747a30704604eade48990a15054150d472.

--- src/screens/products/product/ProductDetailScreen/index.tsx
+++ src/screens/products/product/ProductDetailScreen/index.tsx
@@ -14,7 +14,6 @@ import {
   Col,
   Divider,
   Image,
-  Input,
   Modal,
   Popover,
   Row,
@@ -65,8 +64,7 @@ import { StyledComponent } from "./styles";
 import useAuthorization from "hook/useAuthorization";
 import { callApiNative } from "utils/ApiUtils";
 import { productUpdateApi } from "service/product/product.service";
-import { cloneDeep, debounce } from "lodash";
-import { fullTextSearch } from "utils/StringUtils";
+import { cloneDeep } from "lodash";
 import { SupplierResponse } from "model/core/supplier.model";
 import TreeStore from "component/TreeStore";
 import { StoreResponse } from "model/core/store.model";
@@ -101,6 +99,7 @@ const ProductDetailScreen = (props: { setTitle: (value: string) => void }) => {
 
   const [stores, setStores] = useState<Array<StoreResponse>>([]);
   const [valueStores, setValueStores] = useState<Array<number>>([]);
+  const [valueStoresInv, setValueStoresInv] = useState<Array<number>>([]);
   const tabRef = useRef<HTMLDivElement>(null);
   const [activeTab, setActiveTab] = useState<string>(ProductDetailTabName.INVENTORY);
   const { id, variantId } = useParams<ProductParams>();
@@ -108,7 +107,6 @@ const ProductDetailScreen = (props: { setTitle: (value: string) => void }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isLoadingVariantUpdate, setIsLoadingVariantUpdate] = useState(false);
   const [isLoadingSwitch, setIsLoadingSwitch] = useState(false);
-  const [keySearch, setKeySearch] = useState<string>();
 
   const [isLoadingVariant, setIsLoadingVariant] = useState(false);
   const [active, setActive] = useState<number>(0);
@@ -138,14 +136,6 @@ const ProductDetailScreen = (props: { setTitle: (value: string) => void }) => {
   });
 
   const [dataInventoryOrg, setDataInventoryOrg] = useState<PageResponse<InventoryResponse>>({
-    items: [],
-    metadata: {
-      limit: 30,
-      page: 1,
-      total: 0,
-    },
-  });
-  const [dataHistoryOrg, setDataHistoryOrg] = useState<PageResponse<HistoryInventoryResponse>>({
     items: [],
     metadata: {
       limit: 30,
@@ -345,10 +335,8 @@ const ProductDetailScreen = (props: { setTitle: (value: string) => void }) => {
     setIsLoadingHis(false);
     if (!result) {
       setDataHistory({ ...(initialDataInventory as PageResponse<HistoryInventoryResponse>) });
-      setDataHistoryOrg({ ...(initialDataInventory as PageResponse<HistoryInventoryResponse>) });
     } else {
       setDataHistory(result);
-      setDataHistoryOrg(result);
     }
   }, []);
 
@@ -431,6 +419,18 @@ const ProductDetailScreen = (props: { setTitle: (value: string) => void }) => {
     }
   };
 
+  const changeInvStore = (storeIds: number[]) => {
+    setValueStoresInv(storeIds);
+    if (storeIds.length > 0) {
+      const newVariantInvItems = dataInventoryOrg.items.filter((el: InventoryResponse) =>
+        storeIds.includes(el.store_id),
+      );
+      setDataInventory({ ...dataInventoryOrg, items: [...newVariantInvItems] });
+    } else {
+      setDataInventory(dataInventoryOrg);
+    }
+  };
+
   const getAllStores = async () => {
     callApiNative({ isShowLoading: false }, dispatch, getAllPublicSimpleStoreApi).then((res) => {
       setStores(res);
@@ -509,46 +509,6 @@ const ProductDetailScreen = (props: { setTitle: (value: string) => void }) => {
       tabRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
     }
   }, [tabRef, hash, tab]);
-
-  const debounceSearch = useMemo(
-    () =>
-      debounce((code: string) => {
-        if (activeTab === ProductDetailTabName.INVENTORY) {
-          const variantsInv = cloneDeep(dataInventoryOrg);
-          if (variantsInv.items && variantsInv.items.length === 0) return;
-          if (!code || code === "") {
-            changeDataInventory(1);
-            return;
-          }
-
-          variantsInv.items = variantsInv.items.filter((e) => {
-            return fullTextSearch(code, e.store.toLowerCase());
-          });
-          setDataInventory({ ...dataInventoryOrg, items: [...variantsInv.items] });
-          return;
-        }
-
-        const variantsHis = cloneDeep(dataHistoryOrg);
-        if (variantsHis.items && variantsHis.items.length === 0) return;
-        if (!code || code === "") {
-          changeDataHistory(1);
-          return;
-        }
-
-        variantsHis.items = variantsHis.items.filter((e) => {
-          return fullTextSearch(code, e.store.toLowerCase());
-        });
-        setDataHistory({ ...dataHistoryOrg, items: [...variantsHis.items] });
-      }, 300),
-    [activeTab, dataHistoryOrg, dataInventoryOrg, changeDataHistory, changeDataInventory],
-  );
-
-  const changeKeySearch = useCallback(
-    (code: string) => {
-      debounceSearch(code);
-    },
-    [debounceSearch],
-  );
 
   useEffect(() => {
     if (data?.name) {
@@ -984,24 +944,36 @@ const ProductDetailScreen = (props: { setTitle: (value: string) => void }) => {
                     <Tabs
                       tabBarExtraContent={
                         <>
-                          <TreeStore
-                            name="store_ids"
-                            placeholder="Chọn cửa hàng"
-                            listStore={((stores || []) as StoreResponse[]).filter(
-                              (store) => store.status === enumStoreStatus.ACTIVE,
-                            )}
-                            style={{ minWidth: "220px" }}
-                            value={valueStores}
-                            onChange={onChangeStore}
-                          />
+                          {activeTab === ProductDetailTabName.HISTORY && (
+                            <TreeStore
+                              name="store_ids"
+                              placeholder="Chọn cửa hàng"
+                              listStore={((stores || []) as StoreResponse[]).filter(
+                                (store) => store.status === enumStoreStatus.ACTIVE,
+                              )}
+                              style={{ minWidth: "220px" }}
+                              value={valueStores}
+                              onChange={onChangeStore}
+                            />
+                          )}
+                          {activeTab === ProductDetailTabName.INVENTORY && (
+                            <TreeStore
+                              name="store_inv_ids"
+                              placeholder="Chọn cửa hàng"
+                              listStore={((stores || []) as StoreResponse[]).filter(
+                                (store) => store.status === enumStoreStatus.ACTIVE,
+                              )}
+                              style={{ minWidth: "220px" }}
+                              value={valueStoresInv}
+                              onChange={changeInvStore}
+                            />
+                          )}
                         </>
                       }
                       style={{ overflow: "initial" }}
                       defaultActiveKey={activeTab}
                       onChange={(e) => {
                         setActiveTab(e);
-                        setKeySearch("");
-                        changeKeySearch("");
                       }}
                     >
                       <Tabs.TabPane tab="Danh sách tồn kho" key={ProductDetailTabName.INVENTORY}>
