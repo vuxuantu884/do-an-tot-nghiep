fa9d1b11dde535ff8b121faee53ee0dab12e68d4
LONGCHIN\lvlon <lvlong96k@gmail.com>
2023-02-14 10:57:28 +0700

    USC-213 [Cải tiến] Cải tiến gọi api check tồn kho khi thêm sản phẩm mới Khi tạo đơn, cập nhật & đổi hàng

--- src/screens/order-online/modal/InventoryModal/suggest-inventory.modal.tsx
+++ src/screens/order-online/modal/InventoryModal/suggest-inventory.modal.tsx
@@ -1,5 +1,5 @@
 import { SearchOutlined } from "@ant-design/icons";
-import { Button, Col, Input, Modal, Radio, Row } from "antd";
+import { Button, Col, Input, Modal, Radio, Row, Spin } from "antd";
 import { StoreResponse } from "model/core/store.model";
 import { OrderLineItemRequest } from "model/request/order.request";
 import React, { useCallback, useEffect, useState } from "react";
@@ -17,6 +17,7 @@ type SuggestInventoryModalProps = {
   inventoryArray: Array<any> | null;
   storeArrayResponse: Array<StoreResponse> | null;
   handleCancel: () => void;
+  isLoading?: boolean;
 };
 
 const SuggestInventoryModal: React.FC<SuggestInventoryModalProps> = (
@@ -72,6 +73,7 @@ const SuggestInventoryModal: React.FC<SuggestInventoryModalProps> = (
         <div className="customer-title">
           <span className="title">Kiểm tra thông tin tồn kho</span>
           <Input
+            disabled={props.isLoading}
             placeholder="Tìm kiếm kho"
             allowClear
             onChange={(tg) => {
@@ -93,116 +95,118 @@ const SuggestInventoryModal: React.FC<SuggestInventoryModalProps> = (
       okButtonProps={{ hidden: true }}
     >
       <StyledComponent>
-        <Row gutter={24} className="margin-top-10">
-          <Col md={24}>
-            <div className="overflow-table">
-              <table className="rules">
-                <thead
-                  id="stickyRowProduct"
-                  style={{ top: -1, background: grayF5Color }}
-                  className="tableElementSticky"
-                >
-                  <tr>
-                    <th className="condition-button"></th>
-                    <th className="condition">Sản phẩm</th>
-                    {columnsItem?.map((data, index) => (
-                      <th className="condition" key={index}>
-                        {data.variant}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody
-                  style={{ top: rowProductHeight - 5, background: "white" }}
-                  id="stickyRowCustomer"
-                  className="tableElementSticky"
-                >
-                  <tr>
-                    <td className="condition-button"></td>
-                    <td className="condition">Khách đặt</td>
-                    {columnsItem?.map((data, index) => (
-                      <td className="condition" key={index}>
-                        {data.quantity}
-                      </td>
-                    ))}
-                  </tr>
-                </tbody>
-                <thead
-                  style={{
-                    top: rowProductHeight - 8 + rowHeight,
-                    background: grayF5Color,
-                  }}
-                  className="tableElementSticky"
-                >
-                  <tr>
-                    <td className="condition-button"></td>
-                    <th className="condition">Tổng có thế bán</th>
-                    {columnsItem?.map((data, index) => (
-                      <th className="condition" key={index}>
-                        {setAllAvailable(data.variant_id)}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-
-                <tbody>
-                  {storeData?.map((item, index) => (
-                    <tr key={index}>
-                      <td className="condition-button">
-                        <Button
-                          size="small"
-                          type="primary"
-                          ghost
-                          onClick={() => {
-                            if (item.store_id) onChangeStore(item.store_id);
-                            setVisible(false);
-                          }}
-                          disabled={storeId === item.store_id}
-                        >
-                          Chọn kho
-                        </Button>
-                      </td>
-                      <th
-                        className={storeId === item.store_id ? "condition active" : "condition"}
-                        key={index}
-                      >
-                        {item.store}{" "}
-                        <span style={{ color: dangerColor }}>
-                          {index === 0 ? " - Kho gợi ý" : ""}
-                        </span>
-                      </th>
-
-                      {columnsItem?.map((_itemi, index) => {
-                        let variantDetails = item.variant_inventories.find(
-                          (i: any) => i.variant_id === _itemi.variant_id,
-                        );
-                        if (!variantDetails || !variantDetails.available) {
-                          variantDetails = {
-                            ...variantDetails,
-                            available: 0,
-                          };
-                        }
-                        return (
-                          <td
-                            className="condition"
-                            key={_itemi.variant_id}
-                            style={
-                              variantDetails.available < _itemi.quantity
-                                ? { color: dangerColor }
-                                : { color: successColor }
-                            }
-                          >
-                            {variantDetails.available}
-                          </td>
-                        );
-                      })}
+        <Spin spinning={props.isLoading} tip="đang tải dữ liệu ...">
+          <Row gutter={24} className="margin-top-10">
+            <Col md={24}>
+              <div className="overflow-table">
+                <table className="rules">
+                  <thead
+                    id="stickyRowProduct"
+                    style={{ top: -1, background: grayF5Color }}
+                    className="tableElementSticky"
+                  >
+                    <tr>
+                      <th className="condition">Sản phẩm</th>
+                      {columnsItem?.map((data, index) => (
+                        <th className="condition" key={index}>
+                          {data.variant}
+                        </th>
+                      ))}
+                      <th className="condition-button"></th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </Col>
-        </Row>
+                  </thead>
+                  <tbody
+                    style={{ top: rowProductHeight - 5, background: "white" }}
+                    id="stickyRowCustomer"
+                    className="tableElementSticky"
+                  >
+                    <tr>
+                      <td className="condition">Khách đặt</td>
+                      {columnsItem?.map((data, index) => (
+                        <td className="condition" key={index}>
+                          {data.quantity}
+                        </td>
+                      ))}
+                      <td className="condition-button"></td>
+                    </tr>
+                  </tbody>
+                  <thead
+                    style={{
+                      top: rowProductHeight - 8 + rowHeight,
+                      background: grayF5Color,
+                    }}
+                    className="tableElementSticky"
+                  >
+                    <tr>
+                      <th className="condition">Tổng có thế bán</th>
+                      {columnsItem?.map((data, index) => (
+                        <th className="condition" key={index}>
+                          {setAllAvailable(data.variant_id)}
+                        </th>
+                      ))}
+                      <th className="condition-button"></th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {storeData?.map((item, index) => (
+                      <tr key={index}>
+                        <th
+                          className={storeId === item.store_id ? "condition active" : "condition"}
+                          key={index}
+                        >
+                          {item.store}{" "}
+                          <span style={{ color: dangerColor }}>
+                            {index === 0 ? " - Kho gợi ý" : ""}
+                          </span>
+                        </th>
+
+                        {columnsItem?.map((_itemi, index) => {
+                          let variantDetails = item.variant_inventories.find(
+                            (i: any) => i.variant_id === _itemi.variant_id,
+                          );
+                          if (!variantDetails || !variantDetails.available) {
+                            variantDetails = {
+                              ...variantDetails,
+                              available: 0,
+                            };
+                          }
+                          return (
+                            <td
+                              className="condition"
+                              key={_itemi.variant_id}
+                              style={
+                                variantDetails.available < _itemi.quantity
+                                  ? { color: dangerColor }
+                                  : { color: successColor }
+                              }
+                            >
+                              {variantDetails.available}
+                            </td>
+                          );
+                        })}
+                        <td className="condition-button">
+                          <Button
+                            size="small"
+                            type="primary"
+                            ghost
+                            onClick={() => {
+                              if (item.store_id) onChangeStore(item.store_id);
+                              setVisible(false);
+                            }}
+                            disabled={storeId === item.store_id}
+                          >
+                            Chọn kho
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </Col>
+          </Row>
+        </Spin>
       </StyledComponent>
     </Modal>
   );
