c4c5ae087a7b4b4b4d0beaaac36fcfeaa7e8d265
tuvx <vuxuantuyody@gmail.com>
2023-03-02 08:38:16 +0700

    [ICS-162] PO | Thay đổi thao tác từ trang danh sách -> chi tiết

--- src/screens/purchase-order/purchase-order-list.screen.tsx
+++ src/screens/purchase-order/purchase-order-list.screen.tsx
@@ -363,7 +363,6 @@ const PurchaseOrderListScreen: React.FC<PurchaseOrderListScreenProps> = (
               <Link
                 to={`${UrlConfig.PURCHASE_ORDERS}/${record.id}`}
                 style={{ fontWeight: 500, fontSize: 16, marginRight: 4 }}
-                target="_blank"
               >
                 {value}
               </Link>
