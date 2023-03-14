fe6541b9f1a98ce7bf4a5e61a2da76acd9cdcbbc
binh.phan <phanthanhbinh296@gmail.com>
2023-03-14 18:03:19 +0700

    [SCF-259] add feature config auto reply css

--- src/screens/ecommerce/config/auto-reply/auto-reply-modal.styled.tsx
+++ src/screens/ecommerce/config/auto-reply/auto-reply-modal.styled.tsx
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const ModalStyled = styled.div``;
+export const ModalStyled = styled.div`
+  .actions {
+    display: flex;
+    button {
+      &:not(:last-child) {
+        margin-right: 20px;
+      }
+      border-color: #2a2a86;
+      background-color: #f3f3ff;
+      color: #2a2a86;
+    }
+  }
+`;
